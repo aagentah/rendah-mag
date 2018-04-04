ab1a52273b8695012744a4f7c9025f63f05574a5
dan jones <dannyjones360@gmail.com>
2018-04-05 00:06:18 +0100

    removed "get in touch"

--- src/containers/Pages/Contact/index.js
+++ src/containers/Pages/Contact/index.js
@@ -17,16 +17,16 @@ export class Contact extends PureComponent {
     return (
       <main className="page-fade-in">
         <Helmet title="Contact" />
-        <div className="container  tal">
+        <div className="container  tal  ph4-sm">
           <div className="row">
             <div className="col-sm-14">
-              <h1 className="black  title-font  t5  pt5  pv4">Contact</h1>
+              <h1 className="black  title-font  t5  pt5  pt4-sm  pv4  pb3-sm">Contact</h1>
               <p>Get involved. We&apos;re constantly on the look out for new members for the Rendah team.</p>
               <ul>
-                <li className="pv1">Are you a listener and want to start writing articles? - <strong>Get involved</strong></li>
-                <li className="pv1">Are you a developer and want to help us code the site/side projects? - <strong>Get involved</strong></li>
-                <li className="pv1">Are you an artist and want a Rendah feature/review/interview? - <strong>Get involved</strong></li>
-                <li className="pv1">Are you a visual artist and want to promote artwork on the site? - <strong>Get involved</strong></li>
+                <li className="pv1">Are you a listener and want to start writing articles?</li>
+                <li className="pv1">Are you a developer and want to help us code the site/side projects?</li>
+                <li className="pv1">Are you an artist and want a Rendah feature/review/interview?</li>
+                <li className="pv1">Are you a visual artist and want to promote artwork on the site?</li>
               </ul>
               <p>Even if you&apos;re none of the above and want to get involved, we encourage you to get in touch!<br /><br /></p>
               <p>Please email: <a href="mailto:info@rendahmag.com">info@rendahmag.com</a></p>
