887a8a0ad2b0be2cb145ffac15594d5d45c0cbe4
dan jones <dannyjones360@gmail.com>
2018-04-05 16:17:05 +0100

    add apple-touch-icon.png

--- src/utils/Html.js
+++ src/utils/Html.js
@@ -23,7 +23,7 @@ const Html = ({ store, htmlContent }: Props): Element<'html'> => {
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" />
 
         {head.title.toComponent()}
