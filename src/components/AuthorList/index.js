a6c333f69697420d1ed6116e5e0d3e7c40e2f0e3
dan jones <dannyjones360@gmail.com>
2018-04-09 15:25:25 +0100

    ammend author list title positioning

--- src/components/AuthorList/index.js
+++ src/components/AuthorList/index.js
@@ -14,20 +14,18 @@ export class AuthorList extends PureComponent {
       <div className="container  mv4">
         <div className="row">
           {this.props.list.map((author, i) => (
-            <Link to={`/Author/${author.name.replace(/\s+/g, '-')}`} key={i} className="link  w-100  zoom-in-fade-in-iteration--cont">
-              <div className="col-sm-8  col-md-6  pv3  authorList__col">
-
-                <figure className="rel  pb3">
-                  <div className="shadow2  authorList__img--cont">
-                    <img className="mb3  center  zoom-in-fade-in-iteration--item  authorList__img" alt={author.name} src={`http://res.cloudinary.com/dzz8ji5lj/image/upload/${author.img}`} />
-                  </div>
-                </figure>
-                <h2 className="authorList__title  link  dark-grey  t7  pt1  tac  cp  title-font">
-                  <span>{author.name}</span>
-                  <br />
-                  <span className="t8  grey">({author.alias})</span></h2>
-              </div>
-            </Link>
+            <div key={i} className="col-sm-8  col-md-6  pv3  authorList__col">
+              <figure className="rel  pb3  link  w-100  zoom-in-fade-in-iteration--cont">
+                <Link to={`/Author/${author.name.replace(/\s+/g, '-')}`} className="shadow2  authorList__img--cont  db">
+                  <img className="mb3  center  zoom-in-fade-in-iteration--item  authorList__img" alt={author.name} src={`http://res.cloudinary.com/dzz8ji5lj/image/upload/${author.img}`} />
+                </Link>
+              </figure>
+              <Link to={`/Author/${author.name.replace(/\s+/g, '-')}`} className="authorList__title  link  dark-grey  t7  pt1  tac  cp  title-font">
+                <span>{author.name}</span>
+                <br />
+                <span className="t8  grey">({author.alias})</span>
+              </Link>
+            </div>
           ))}
         </div>
       </div>
