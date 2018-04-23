315dde2da8b2fbde394a251df83783490e0aeb38
dan jones <dannyjones360@gmail.com>
2018-04-23 21:43:14 +0100

    ammends to author SEO

--- src/containers/Pages/Author/index.js
+++ src/containers/Pages/Author/index.js
@@ -21,7 +21,7 @@ export class Author extends PureComponent {
       <main className="page-fade-in">
         <Helmet title={this.props.match.params.id.replace(/\s+/g, '-')} />
         <h1 className="dn">{this.props.match.params.id.replace(/\s+/g, '-')}</h1>
-        <AuthorInfo match={this.props.match} seo={'true'} />
+        <AuthorInfo match={this.props.match} seo />
         <p className="tac  mb2  mt3">Latest from {this.props.match.params.id.split('-')[0]}</p>
         <AuthorArticles match={this.props.match} />
       </main>
