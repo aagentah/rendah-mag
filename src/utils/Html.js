39028389579d62014d4215b151237af7645248ae
dan jones <dannyjones360@gmail.com>
2018-05-23 09:51:42 +0100

    add new favicons

--- src/utils/Html.js
+++ src/utils/Html.js
@@ -23,8 +23,15 @@ const Html = ({ store, htmlContent }: Props): Element<'html'> => {
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+
         <meta name="description" content="Beats, Halftime & Future Bass Magazine" />
         <meta property="fb:app_id" content="154881868603516" />
         <meta property="og:type" content="website" />
