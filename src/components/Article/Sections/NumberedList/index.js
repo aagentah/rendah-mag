930a9caed2701fbb3881e171e20f4e727e1b6b88
dan jones <dannyjones360@gmail.com>
2018-05-26 13:18:03 +0100

    add text field to List sections & slight spacing ammends

--- src/components/Article/Sections/NumberedList/index.js
+++ src/components/Article/Sections/NumberedList/index.js
@@ -6,24 +6,34 @@ import PropTypes from 'prop-types';
 
 
 export class NumberedList extends PureComponent {
+  text = () => {
+    if (this.props.text) {
+      return <p className="pv1  normal-font  grey">{this.props.text}</p>;
+    }
+    return null;
+  };
+
   render() {
     return (
       <div>
-        <ul>
+        {this.text()}
+        <ol className="mb0">
           {this.props.list.map((item, i) => (
             <li className="pv1  normal-font  grey" key={i}><p>{item}</p></li>
           ))}
-        </ul>
+        </ol>
       </div>
     );
   }
 }
 
 NumberedList.propTypes = {
+  text: PropTypes.string,
   list: PropTypes.arrayOf(PropTypes.string),
 };
 
 NumberedList.defaultProps = {
+  text: '',
   list: [
     '',
   ],
