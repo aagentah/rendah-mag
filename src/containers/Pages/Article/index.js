41731f30921ed668296477b4fdc31ac3920d9e43
dan jones <dannyjones360@gmail.com>
2018-05-26 22:50:21 +0100

    article spacing ammends and added comments panels

--- src/containers/Pages/Article/index.js
+++ src/containers/Pages/Article/index.js
@@ -22,14 +22,16 @@ export class Article extends PureComponent {
         <ArticleInfo match={this.props.match} />
         <div className="container  tac">
           <div className="row  rel">
+            <div className="facebook-comments__panel--top" />
             <div className="col-md-16  col-md-offset-4  facebook-comments">
               <FacebookProvider appId="154881868603516">
-                <Comments href={`https://www.rendahmag.com${this.props.match.url}`} numPosts={3} width="100%" />
+                <Comments href={`https://www.rendahmag.com${this.props.match.url}`} numPosts={3} width="100%" mobile />
               </FacebookProvider>
             </div>
+            <div className="facebook-comments__panel--bottom" />
           </div>
         </div>
-        <h2 className="tac  mb4">More articles</h2>
+        <h2 className="tac  mv4">More articles</h2>
         <ExtraArticles />
       </main>
     );
