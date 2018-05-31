3867d68533817998d6cd95734d9346ce5aa46913
dan jones <dannyjones360@gmail.com>
2018-05-31 17:39:47 +0100

    added social icons to article__link

--- src/components/Article/Sections/Link/index.js
+++ src/components/Article/Sections/Link/index.js
@@ -10,7 +10,13 @@ export class Link extends PureComponent {
     if (this.props.linkType) {
       switch (this.props.linkType) {
         case 'Soundcloud':
-          return <span className="pv1  normal-font  grey">{this.props.linkType}</span>;
+          return <img className="mr2  di" width="25" src="https://res.cloudinary.com/dzz8ji5lj/image/upload/c_scale,w_25/v1527784080/brand/social/iconmonstr-soundcloud-5.png" alt={this.props.linkType} role="presentation" />;
+        case 'Twitter':
+          return <img className="mr2  di" width="25" src="https://res.cloudinary.com/dzz8ji5lj/image/upload/c_scale,w_25/v1527784080/brand/social/iconmonstr-twitter-5.png" alt={this.props.linkType} role="presentation" />;
+        case 'Youtube':
+          return <img className="mr2  di" width="25" src="https://res.cloudinary.com/dzz8ji5lj/image/upload/c_scale,w_25/v1527784080/brand/social/iconmonstr-youtube-5.png" alt={this.props.linkType} role="presentation" />;
+        case 'Instagram':
+          return <img className="mr2  di" width="25" src="https://res.cloudinary.com/dzz8ji5lj/image/upload/c_scale,w_25/v1527784080/brand/social/iconmonstr-instagram-5.png" alt={this.props.linkType} role="presentation" />;
         default:
           return null;
       }
@@ -21,9 +27,9 @@ export class Link extends PureComponent {
   render() {
     return (
       <div>
-        <a className="link  rendah-red  normal-font" href={this.props.url} target="_blank">
-          {this.props.text}
+        <a className="link  black  normal-font" href={this.props.url} target="_blank">
           {this.linkType()}
+          <span className="Article__link--span  pb2">{this.props.text}</span>
         </a>
       </div>
     );
