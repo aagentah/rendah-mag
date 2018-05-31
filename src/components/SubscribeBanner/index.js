c43906de9c5f3c87cf8aa7e0868a30d89dbc8c36
dan jones <dannyjones360@gmail.com>
2018-05-31 13:09:37 +0100

    minor positioning updates

--- src/components/SubscribeBanner/index.js
+++ src/components/SubscribeBanner/index.js
@@ -11,7 +11,7 @@ export class SubscribeBanner extends PureComponent {
     const formProps = {
       action: 'https://rendahmag.us17.list-manage.com/subscribe/post?u=df0d549f92845c8dfc4d99dde&amp;id=2904b740be',
       messages: {
-        inputPlaceholder: 'ENTER YOUR EMAIL',
+        inputPlaceholder: 'SUBSCRIBE TO RENDAH WEEKLY',
         btnLabel: 'SUBSCRIBE',
         sending: 'SUBSCRIBING',
         success: 'SUBSCRIBED',
