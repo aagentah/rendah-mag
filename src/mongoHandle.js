221cf7d7ec54bd20188cae30a5c9997d5898890a
dan jones <dannyjones360@gmail.com>
2018-06-05 14:06:04 +0100

    Added mongoHandle to split out server.js

---
+++ src/mongoHandle.js
@@ -0,0 +1,250 @@
+import MongoClient from 'mongodb';
+
+const mongoHandle = (app) => {
+  // connect to mongo db
+  let db;
+  const mongoUsername = process.env.MONGO_USERNAME;
+  const mongoPassword = process.env.MONGO_PASSWORD;
+
+  if (__DEV__) {
+    MongoClient.connect('mongodb://Rendah-staging:test@ds123930.mlab.com:23930/halftimefront', (err, database) => {
+      if (err) return console.log(err);
+      db = database;
+      return console.log('staging db connected');
+    });
+  } else {
+    MongoClient.connect(`mongodb://${mongoUsername}:${mongoPassword}@ds019996.mlab.com:19996/rendah`, (err, database) => {
+      if (err) return console.log(err);
+      db = database;
+      return console.log('production db connected');
+    });
+  }
+
+
+  // GET week
+  app.get('/api/week', (req, res) => {
+    let articles = [];
+
+    db.collection('articles')
+      .find()
+      .limit(2)
+      .sort('created', -1)
+      .toArray()
+      .then((result) => {
+        articles = articles.concat(result);
+      })
+      .then(() => {
+        // console.log(articles);
+        res.send(articles);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  });
+
+  // GET articles
+  app.get('/api/articles', (req, res) => {
+    let articles = [];
+
+    db.collection('articles')
+      .find()
+      .skip(2)
+      .limit(12)
+      .sort('created', -1)
+      .toArray()
+      .then((result) => {
+        articles = articles.concat(result);
+      })
+      .then(() => {
+        // console.log(articles);
+        res.send(articles);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  });
+
+  // GET authorArticles
+  app.get('/api/authorArticles', (req, res) => {
+    let articles = [];
+    let param = req.query.authorQuery;
+    param = param.replace(/-/g, ' ');
+
+    db.collection('articles')
+      // .find()
+      .find({
+        author: {
+          $regex: `.*${param}.*`,
+        },
+      })
+      .limit(12)
+      .sort('created', -1)
+      .toArray()
+      .then((result) => {
+        articles = articles.concat(result);
+      })
+      .then(() => {
+        // console.log(articles);
+        res.send(articles);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  });
+
+  // GET extra
+  app.get('/api/extra', (req, res) => {
+    let articles = [];
+
+    db.collection('articles')
+      .aggregate([{
+        $sample: {
+          size: 4,
+        },
+      }])
+      .toArray()
+      .then((result) => {
+        articles = articles.concat(result);
+      })
+      .then(() => {
+        // console.log(articles);
+        res.send(articles);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  });
+
+  // GET authors
+  app.get('/api/authors', (req, res) => {
+    let authors = [];
+
+    db.collection('authors')
+      .find()
+      .limit(24)
+      .sort('name', 1)
+      .toArray()
+      .then((result) => {
+        // console.log(result);
+        authors = authors.concat(result);
+      })
+      .then(() => {
+        res.send(authors);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  });
+
+  // GET search
+  app.get('/api/search', (req, res) => {
+    let articles = [];
+
+    db.collection('articles')
+      .find({
+        $or: [{
+            title: {
+              $regex: `.*${req.query.searchQuery}.*`,
+              $options: 'i',
+            },
+          },
+          {
+            description: {
+              $regex: `.*${req.query.searchQuery}.*`,
+              $options: 'i',
+            },
+          },
+          {
+            author: {
+              $regex: `.*${req.query.searchQuery}.*`,
+              $options: 'i',
+            },
+          },
+        ],
+      })
+      .limit(24)
+      .sort('created', -1)
+      .toArray()
+      .then((result) => {
+        articles = articles.concat(result);
+      })
+      .then(() => {
+        // console.log(articles);
+        res.send(articles);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  });
+
+  // GET category
+  app.get('/api/category', (req, res) => {
+    let articles = [];
+
+    db.collection('articles')
+      .find({
+        $or: [{
+          category: {
+            $regex: `.*${req.query.categoryQuery}.*`,
+          },
+        }, ],
+      })
+      .limit(12)
+      .sort('created', -1)
+      .toArray()
+      .then((result) => {
+        articles = articles.concat(result);
+      })
+      .then(() => {
+        // console.log(articles);
+        res.send(articles);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  });
+
+  // GET article
+  app.get('/api/article', (req, res) => {
+    let article = {};
+    const param = req.query.title;
+    // param = param.replace(/-/g, ' ');
+
+    db.collection('articles')
+      .findOne({
+        url: param,
+      })
+      .then((result) => {
+        article = result;
+      })
+      .then(() => {
+        res.send(article);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  });
+
+  // GET author
+  app.get('/api/author', (req, res) => {
+    let article = {};
+    let param = req.query.title;
+    param = param.replace(/-/g, ' ');
+
+    db.collection('authors')
+      .findOne({
+        name: param,
+      })
+      .then((result) => {
+        article = result;
+      })
+      .then(() => {
+        res.send(article);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  });
+}
+
+export default mongoHandle;
