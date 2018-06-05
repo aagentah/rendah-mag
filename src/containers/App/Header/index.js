3f2db0ae7882794395a01aa26317bc0fb5974ea6
dan jones <dannyjones360@gmail.com>
2018-06-05 21:35:23 +0100

    Add mobile nav carousel

--- src/containers/App/Header/index.js
+++ src/containers/App/Header/index.js
@@ -1,61 +1,99 @@
-/* eslint-disable import/no-named-as-default, max-len */
+/* eslint-disable import/no-named-as-default, max-len, react/prefer-stateless-function */
 
-import React from 'react';
-import type { Element } from 'react';
+import React, { PureComponent } from 'react';
 import { Link } from 'react-router-dom';
+import Slider from 'react-slick';
 
 import SearchInput from '../../../components/SearchInput';
 
-const Header = (): Element<'div'> => (
-  <div className="rel  shadow2  z9">
-    <header role="banner">
-      <Link className="link" to={'/'}>
-        <img className="logo" width="50" src={require('../assets/Rendah-Logo-Small.png')} alt="Logo" role="presentation" />
-      </Link>
-      <nav className="nav  tal  pt4">
-        <ul className="nav__list  ma0  pa0  tac  center  rel">
-          <li className="dib">
-            <Link className="nav__link  title-font  black  dib  ph2  t6  link" to={'/'}>Home</Link>
-          </li>
-          <li className="dib">
-            <Link className="nav__link  title-font  black  dib  ph2  t6  link" to={'/Authors'}>Authors</Link>
-          </li>
-          <li className="dib">
-            <Link className="nav__link  title-font  black  dib  ph2  t6  link" to={'/GetInvolved'}>Get Involved</Link>
-          </li>
-          <li data-nav-category="1" className="abs  dn-md">
-            <Link className="nav__link  title-font  black  dib  ph2  t6  link" to={'/Category/Interviews'}>Interviews</Link>
-          </li>
-          <li data-nav-category="2" className="abs  dn-md">
-            <Link className="nav__link  title-font  black  dib  ph2  t6  link" to={'/Category/Insights'}>Insights</Link>
-          </li>
-          <li data-nav-category="3" className="abs  dn-md">
-            <Link className="nav__link  title-font  black  dib  ph2  t6  link" to={'/Category/News'}>News</Link>
-          </li>
-        </ul>
-      </nav>
-      <div className="header__social--cont  pr0-sm">
-        <SearchInput textAlign="inherit" />
-        <a className="header__social  ph1  dn-sm" href="https://www.facebook.com/rendahmag/" rel="noopener noreferrer" target="_blank">
-          <img src={require('../assets/social/iconmonstr-facebook-5.png')} alt="facebook" />
-        </a>
-        <a className="header__social  ph1  dn-sm" href="https://twitter.com/RendahMag" rel="noopener noreferrer" target="_blank">
-          <img src={require('../assets/social/iconmonstr-twitter-5.png')} alt="twitter" />
-        </a>
-        <a className="header__social  ph1  dn-sm" href="https://www.instagram.com/rendahmag/" rel="noopener noreferrer" target="_blank">
-          <img src={require('../assets/social/iconmonstr-instagram-5.png')} alt="instagram" />
-        </a>
-        {
-          // <a className="header__social  ph1  dn-sm" href="https://www.soundcloud.com/rendahmag/" rel="noopener noreferrer" target="_blank">
-          //   <img src={require('../assets/social/iconmonstr-soundcloud-5.png')} alt="soundcloud" />
-          // </a>
-          // <a className="header__social  ph1  dn-sm" href="https://www.youtube.com/channel/UCFpU3WYYWy5qWSYf306_m3A" rel="noopener noreferrer" target="_blank">
-          //   <img src={require('../assets/social/iconmonstr-youtube-5.png')} alt="youtube" />
-          // </a>
-        }
+export class Header extends PureComponent {
+  render() {
+    const settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 2,
+      prevArrow: <span>{'<<'}</span>,
+      nextArrow: <span>{'>>'}</span>,
+    };
+
+    return (
+      <div className="rel  shadow2  z9">
+        <header role="banner">
+          <Link className="link" to={'/'}>
+            <img className="logo" width="50" src={require('../assets/Rendah-Logo-Small.png')} alt="Logo" role="presentation" />
+          </Link>
+          <nav className="nav  tal  pt4">
+            <ul className="nav__list  ma0  pa0  tac  center  rel">
+              <li className="dib">
+                <Link className="nav__link  title-font  black  dib  ph2  t6  link" to={'/'}>Home</Link>
+              </li>
+              <li className="dib">
+                <Link className="nav__link  title-font  black  dib  ph2  t6  link" to={'/Authors'}>Authors</Link>
+              </li>
+              <li className="dib">
+                <Link className="nav__link  title-font  black  dib  ph2  t6  link" to={'/GetInvolved'}>Get Involved</Link>
+              </li>
+              <li data-nav-category="1" className="abs  dn-sm">
+                <Link className="nav__link  title-font  black  dib  ph2  t6  link" to={'/Category/Interviews'}>Interviews</Link>
+              </li>
+              <li data-nav-category="2" className="abs  dn-sm">
+                <Link className="nav__link  title-font  black  dib  ph2  t6  link" to={'/Category/Insights'}>Insights</Link>
+              </li>
+              <li data-nav-category="3" className="abs  dn-sm">
+                <Link className="nav__link  title-font  black  dib  ph2  t6  link" to={'/Category/News'}>News</Link>
+              </li>
+            </ul>
+          </nav>
+          <div className="header__social--cont  pr0-sm">
+            <SearchInput textAlign="inherit" />
+            <a className="header__social  ph1  dn-sm" href="https://www.facebook.com/rendahmag/" rel="noopener noreferrer" target="_blank">
+              <img src={require('../assets/social/iconmonstr-facebook-5.png')} alt="facebook" />
+            </a>
+            <a className="header__social  ph1  dn-sm" href="https://twitter.com/RendahMag" rel="noopener noreferrer" target="_blank">
+              <img src={require('../assets/social/iconmonstr-twitter-5.png')} alt="twitter" />
+            </a>
+            <a className="header__social  ph1  dn-sm" href="https://www.instagram.com/rendahmag/" rel="noopener noreferrer" target="_blank">
+              <img src={require('../assets/social/iconmonstr-instagram-5.png')} alt="instagram" />
+            </a>
+            {
+              // <a className="header__social  ph1  dn-sm" href="https://www.soundcloud.com/rendahmag/" rel="noopener noreferrer" target="_blank">
+              //   <img src={require('../assets/social/iconmonstr-soundcloud-5.png')} alt="soundcloud" />
+              // </a>
+              // <a className="header__social  ph1  dn-sm" href="https://www.youtube.com/channel/UCFpU3WYYWy5qWSYf306_m3A" rel="noopener noreferrer" target="_blank">
+              //   <img src={require('../assets/social/iconmonstr-youtube-5.png')} alt="youtube" />
+              // </a>
+            }
+          </div>
+        </header>
+        <div className="dn  db-sm  bg-black">
+          <div className="w-70  center  white">
+            <Slider {...settings}>
+              <div>
+                <Link className="db  tac  white  link  pv2" to={'/'}>Home</Link>
+              </div>
+              <div>
+                <Link className="db  tac  white  link  pv2" to={'/Authors'}>Authors</Link>
+              </div>
+              <div>
+                <Link className="db  tac  white  link  pv2" to={'/GetInvolved'}>Contact</Link>
+              </div>
+              <div>
+                <Link className="db  tac  white  link  pv2" to={'/Category/Interviews'}>Interviews</Link>
+              </div>
+              <div>
+                <Link className="db  tac  white  link  pv2" to={'/Category/Insights'}>Insights</Link>
+              </div>
+              <div>
+                <Link className="db  tac  white  link  pv2" to={'/Category/News'}>News</Link>
+              </div>
+            </Slider>
+          </div>
+        </div>
       </div>
-    </header>
-  </div>
-);
+    );
+  }
+}
 
 export default Header;
