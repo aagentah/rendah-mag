7ebd1384def06d9347d4b65c297c606a7afc9722
dan jones <dannyjones360@gmail.com>
2018-07-05 14:32:47 +0100

    Ammend categry grid links

--- src/components/CategoryGrid/index.js
+++ src/components/CategoryGrid/index.js
@@ -11,21 +11,21 @@ export class CategoryGrid extends PureComponent {
         <div className="container  pt3  pb5">
           <div className="row">
             <div className="col-sm-24  col-lg-8">
-              <Link className="link" to={'/category/Interviews'}>
+              <Link className="link" to={'/category/interviews'}>
                 <div className="mv3  shadow2  CategoryGrid__bg" style={{ backgroundImage: 'url(https://res.cloudinary.com/dzz8ji5lj/image/upload/q_auto:good/categories/interviews.jpg)' }}>
                   <h4 className="rel  tac  white  t6  title-font  bg-black  pv1  CategoryGrid__title">Interviews</h4>
                 </div>
               </Link>
             </div>
             <div className="col-sm-24  col-lg-8">
-              <Link className="link" to={'/category/Insights'}>
+              <Link className="link" to={'/category/insights'}>
                 <div className="mv3  shadow2  CategoryGrid__bg" style={{ backgroundImage: 'url(https://res.cloudinary.com/dzz8ji5lj/image/upload/q_auto:good/categories/insights.jpg)' }}>
                   <h4 className="rel  tac  white  t6  title-font  bg-black  pv1  CategoryGrid__title">Insights</h4>
                 </div>
               </Link>
             </div>
             <div className="col-sm-24  col-lg-8">
-              <Link className="link" to={'/category/News'}>
+              <Link className="link" to={'/category/news'}>
                 <div className="mv3  shadow2  CategoryGrid__bg" style={{ backgroundImage: 'url(https://res.cloudinary.com/dzz8ji5lj/image/upload/q_auto:good/categories/news.jpg)' }}>
                   <h4 className="rel  tac  white  t6  title-font  bg-black  pv1  CategoryGrid__title">News</h4>
                 </div>
