bfba0a722930f49d736ddaeced0e8ddef306a848
dan jones <dannyjones360@gmail.com>
2018-07-09 16:03:56 +0100

    Remove static feeds & replace with dynamic ones

--- src/mongoHandle.js
+++ src/mongoHandle.js
@@ -1,4 +1,5 @@
 import MongoClient from 'mongodb';
+import builder from 'xmlbuilder';
 
 const mongoHandle = (app) => {
   // connect to mongo db
@@ -20,6 +21,25 @@ const mongoHandle = (app) => {
     });
   }
 
+  const generateXML = () => {
+    const minutes = 1;
+    const interval = minutes * 60 * 1000;
+
+    setInterval(() => {
+
+      // var sitemap = builder.create('urlset')
+      // .ele('xmlbuilder')
+      //   .att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9')
+      //   .att('xmlns:news', 'http://www.google.com/schemas/sitemap-news/0.9')
+      //   .ele('url')
+      //     .ele('loc')
+      // .end({ pretty: true});
+
+    }, interval);
+  }
+
+  // generateXML();
+
   // Checks if article created date is before now
   const checkDateAndReturn = (articles, limit) => {
 
@@ -28,7 +48,7 @@ const mongoHandle = (app) => {
     let article;
 
     Date.prototype.addHours = function(h) {
-      this.setHours(this.getHours()+h);
+      this.setHours(this.getHours() + h);
       return this;
     }
 
@@ -55,6 +75,152 @@ const mongoHandle = (app) => {
 
     return data;
   }
+
+  app.get('/feeds/sitemap.xml', function(req, res) {
+
+    let articles = [];
+    var sitemap = [
+      'https://www.rendahmag.com/',
+      'https://www.rendahmag.com/get-involved',
+      'https://www.rendahmag.com/watch-tower',
+      'https://www.rendahmag.com/category/interviews',
+      'https://www.rendahmag.com/category/insights',
+      'https://www.rendahmag.com/category/news',
+      'https://www.rendahmag.com/authors',
+      'https://www.rendahmag.com/authors/dan-jones',
+      'https://www.rendahmag.com/authors/dayle-hamers',
+      'https://www.rendahmag.com/authors/harry-taylor',
+      'https://www.rendahmag.com/authors/kieran-may',
+      'https://www.rendahmag.com/authors/sam-langley',
+    ]
+
+    db.collection('articles')
+      .find()
+      .sort('created', -1)
+      .toArray()
+      .then((result) => {
+        articles = articles.concat(result);
+      })
+      .then(() => {
+        const data = checkDateAndReturn(articles, 12);
+        var i;
+        for (i = 0; i < data.length; i++) {
+          sitemap.push(`https://www.rendahmag.com/article/${data[i].url}`);
+        }
+
+        var sitemapXML = builder.create('urlset')
+          .ele('xmlbuilder')
+          .att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9')
+          .att('xmlns:news', 'http://www.google.com/schemas/sitemap-news/0.9');
+
+        var i;
+        for (i = 0; i < sitemap.length; i++) {
+          var item = builder.create('url').ele('loc').txt(sitemap[i]);
+          sitemapXML.importDocument(item);
+        }
+
+        console.log(sitemapXML.toString({
+          pretty: true
+        }));
+
+        res.header('Content-Type', 'application/xml');
+        res.send(sitemapXML.toString({
+          pretty: true
+        }));
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  });
+
+  app.get('/feeds/articles.xml', function(req, res) {
+
+    let articles = [];
+
+    db.collection('articles')
+      .find()
+      .limit(5)
+      .sort('created', -1)
+      .toArray()
+      .then((result) => {
+        articles = articles.concat(result);
+      })
+      .then(() => {
+        const data = checkDateAndReturn(articles, 5);
+
+        var articlesFeedXML = builder.create('rss');
+        articlesFeedXML.att({
+          'version': "2.0",
+          'xmlns:atom': "http://www.w3.org/2005/Atom",
+          'xmlns:content': "http://purl.org/rss/1.0/modules/content/",
+          'xmlns:media': "http://search.yahoo.com/mrss/",
+        });
+
+        var articlesFeedBody = builder.create('channel');
+
+        var a = builder.create('title').txt('Rendah Weekly');
+        var b = builder.create('link').txt('https://www.rendahmag.com/');
+        var c = builder.create('atom:link')
+          .att('href', 'https://www.rendahmag.com/feeds/articles.xml')
+          .att('rel', 'self')
+          .att('type', 'application/rss+xml');
+        var d = builder.create('description').txt('Weekly updates from Rendah');
+
+        articlesFeedBody.importDocument(a);
+        articlesFeedBody.importDocument(b);
+        articlesFeedBody.importDocument(c);
+        articlesFeedBody.importDocument(d);
+
+        var i;
+        for (i = 0; i < data.length; i++) {
+
+          function pubDate(date) {
+
+            if (typeof date === 'undefined') {
+              date = new Date();
+            }
+            var pieces = date.toString().split(' '),
+              offsetTime = pieces[5].match(/[-+]\d{4}/),
+              offset = (offsetTime) ? offsetTime : pieces[5],
+              parts = [
+                pieces[0] + ',',
+                pieces[2],
+                pieces[1],
+                pieces[3],
+                pieces[4],
+                offset
+              ];
+
+            return parts.join(' ');
+          }
+
+          var item = builder.create('item')
+            .ele('title').txt(data[i].title.replace("&", "and")).up()
+            .ele('description').txt(data[i].description.replace("&", "and")).up()
+            .ele('link').txt(`https://www.rendahmag.com/article/${data[i].url}`).up()
+            .ele('guid').txt(`https://www.rendahmag.com/article/${data[i].url}`).up()
+            .ele('pubDate').txt(pubDate(`${data[i].created}`)).up()
+            .ele('media:content').txt(pubDate(`${data[i].created}`))
+            .att('url', `https://res.cloudinary.com/dzz8ji5lj/image/upload/c_fill,g_faces:center,h_230,q_auto:eco,w_300/${data[i].img}`)
+            .att('type', 'image/jpg').up()
+          articlesFeedBody.importDocument(item);
+        }
+
+        articlesFeedXML.importDocument(articlesFeedBody);
+
+        console.log(articlesFeedXML.toString({
+          pretty: true
+        }));
+
+        res.header('Content-Type', 'application/xml');
+        res.send(articlesFeedXML.toString({
+          pretty: true
+        }));
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  });
 
 
   // GET week
