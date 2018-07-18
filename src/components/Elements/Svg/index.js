253737ac24e9b5b17deb471109db4abb8760b51f
dan jones <dannyjones360@gmail.com>
2018-07-18 13:47:09 +0100

    Add caret as an SVG

--- src/components/Elements/Svg/index.js
+++ src/components/Elements/Svg/index.js
@@ -110,6 +110,12 @@ export const ArrowRight = (): Element<'div'> => (
   </div>
 );
 
+export const CaretDown = (): Element<'div'> => (
+  <div data-svg="CaretDown">
+    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" /></svg>
+  </div>
+);
+
 export const NULL = ({ text }: Props): Element<'div'> => (
   <div>{text}</div>
 );
