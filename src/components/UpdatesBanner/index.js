c5be9b016cd8c8002fb4fcd87503a57e7427b011
dan jones <dannyjones360@gmail.com>
2018-08-05 21:23:19 +0100

    Update giveaway img quality

--- src/components/UpdatesBanner/index.js
+++ src/components/UpdatesBanner/index.js
@@ -11,7 +11,7 @@ export class UpdatesBanner extends PureComponent {
           <div className="row">
             <div className="col-24">
               <a className="link  db  shadow2  shadow3-hover" href="https://www.facebook.com/rendahmag/photos/a.398209153997157.1073741829.302421033575970/438109933340412/?type=3&theater" rel="noopener noreferrer" target="_blank">
-                <img className="w-100" alt="Giveaway" src="https://res.cloudinary.com/dzz8ji5lj/image/upload/q_auto:good/v1533498843/brand/robert-moog-poster-competition-website.jpg" />
+                <img className="w-100" alt="Giveaway" src="https://res.cloudinary.com/dzz8ji5lj/image/upload/v1533498843/brand/robert-moog-poster-competition-website.jpg" />
               </a>
             </div>
           </div>
