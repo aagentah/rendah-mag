4977d15d209cf123cb1ab05374b3fb6127e88a28
dan jones <dannyjones360@gmail.com>
2018-08-09 21:06:04 +0100

    Add subscribe banner back in & change all mid-grey to dark-grey across site

--- src/containers/Pages/Search/index.js
+++ src/containers/Pages/Search/index.js
@@ -20,7 +20,7 @@ export class Search extends PureComponent {
       <main className="page-fade-in">
         <Helmet title="Search" />
         <h1 className="dn">{this.props.match.params.query}</h1>
-        <p className="tac  mid-grey  t6  ttu  khula-bold  mt3  pt4  pt4-sm  pv4  pb3-sm">Latest results for: {this.props.match.params.query}</p>
+        <p className="tac  dark-grey  t6  ttu  khula-bold  mt3  pt4  pt4-sm  pv4  pb3-sm">Latest results for: {this.props.match.params.query}</p>
         <SearchArticles match={this.props.match} />
         <CategoryGrid />
       </main>
