e6a6b49d19e729691325696f1acaca1385f8e989
dan jones <dannyjones360@gmail.com>
2018-08-13 17:15:46 +0100

    Add modules embed url

--- src/containers/Pages/Mixes/index.js
+++ src/containers/Pages/Mixes/index.js
@@ -25,7 +25,7 @@ export class WatchTower extends PureComponent {
         <div className="col-24  col-md-12  offset-md-6  pb0  pb4-md">
           <div className="w-90  db  center  mb3">
             <Iframe
-              url="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/543806010&color=%23ff817b&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+              url="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/484994280&color=%23ff817b&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
               width="100%"
               height="420"
               display="initial"
