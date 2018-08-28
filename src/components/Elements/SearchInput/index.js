b3b3c2eb39e591c93e5cf65925eaf67e99de454a
dan jones <dannyjones360@gmail.com>
2018-08-28 16:25:49 +0100

    Further progress

--- src/components/Elements/SearchInput/index.js
+++ src/components/Elements/SearchInput/index.js
@@ -47,8 +47,8 @@ export class SearchInput extends PureComponent {
     return (
       <React.Fragment>
         <form className="rel" action="#" onSubmit={this.submitForm}>
-          <input className="t-title  ph2  pb1  w-100  f3  f5-lg  search-input__input" type="search" placeholder={this.state.placeholder} value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)} />
-          <button className="cp  abs  search-input__submit" tyle="submit"><ArrowRight /></button>
+          <input className="t-title  ph2  pb1  w-100  f3  f5-lg  bb bc-black" type="search" placeholder={this.state.placeholder} value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)} />
+          <button className="cp  abs  right" tyle="submit"><ArrowRight /></button>
         </form>
         {this.redir()}
       </React.Fragment>
--- src/components/SubscribeBanner/index.js
+++ src/components/SubscribeBanner/index.js
@@ -9,11 +9,11 @@ import { ArrowRight } from '../Elements/Svg';
 export class SubscribeBanner extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = { inputPlaceHolder: 'ENTER EMAIL TO SUBSCRIBE' };
+    this.state = { inputPlaceHolder: 'ENTER YOUR EMAIL' };
   }
 
   render() {
-    const CustomForm = ({ status, message, onValidated }) => {
+    const CustomForm = ({ status, onValidated }) => {
       let email;
 
       const submit = () =>
@@ -30,37 +30,38 @@ export class SubscribeBanner extends PureComponent {
 
       return (
         <div>
-          <form onSubmit={submitInit} action="" noValidate>
+          <form className="rel" onSubmit={submitInit} action="" noValidate>
             <input
+              className="t-title  ph2  pb1  w-100  f5  bb bc-black"
               ref={node => (email = node)}
               type="email"
               placeholder={this.state.inputPlaceHolder}
             />
-            <span role="button" tabIndex={0} onClick={submitInit}><ArrowRight /></span>
-            <br />
+            <span className="cp  abs  right" role="button" tabIndex={0} onClick={submitInit}><ArrowRight /></span>
           </form>
-          {status === 'sending' && <div className="message">sending...</div>}
+          {status === 'sending' && <div className="message">Subscribing...</div>}
           {status === 'error' && <div className="message">Please enter a valid email...</div>}
-          {status === 'success' && (
-            <div className="message" dangerouslySetInnerHTML={{ __html: message }} />
-          )}
+          {status === 'success' && <div className="message">Welcome to the family.</div>}
         </div>
       );
     };
 
     const url = 'https://rendahmag.us17.list-manage.com/subscribe/post?u=df0d549f92845c8dfc4d99dde&amp;id=2904b740be';
     return (
-      <div className="subscribeBanner">
-        <MailchimpSubscribe
-          url={url}
-          render={({ subscribe, status, message }) => (
-            <CustomForm
-              status={status}
-              message={message}
-              onValidated={formData => subscribe(formData)}
+      <div className="container-medium  center  pv2">
+        <div className="flex">
+          <div className="col-24">
+            <MailchimpSubscribe
+              url={url}
+              render={({ subscribe, status }) => (
+                <CustomForm
+                  status={status}
+                  onValidated={formData => subscribe(formData)}
+                />
+              )}
             />
-          )}
-        />
+          </div>
+        </div>
       </div>
     );
   }
