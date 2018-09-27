ab9f21adf66ab4bd2b575288d97ef7b8651d512b
dan jones <dannyjones360@gmail.com>
2018-09-27 19:26:15 +0100

    Enable double fetching on latest articles

--- src/containers/Fragments/LatestArticles/action.js
+++ src/containers/Fragments/LatestArticles/action.js
@@ -35,25 +35,9 @@ export const fetchLatestArticles = (limit: number) =>
     });
   };
 
-// Preventing dobule fetching data
-/* istanbul ignore next */
-const shouldFetchLatestArticles = (state): boolean => {
-  // In development, we will allow action dispatching
-  // or your reducer hot reloading won't updated on the view
-  if (__DEV__) return true; // Next line = Preventing double fetching data
-  if (state.latestArticles.readyStatus === LATESTARTICLES_SUCCESS) return false;
-  return true;
-};
 
 /* istanbul ignore next */
 export const fetchLatestArticlesIfNeeded = (limit: number) =>
-  (dispatch, getState, axios: any) => {
-    /* istanbul ignore next */
-    if (shouldFetchLatestArticles(getState())) {
-      /* istanbul ignore next */
-      return dispatch(fetchLatestArticles(limit, axios));
-    }
-
-    /* istanbul ignore next */
-    return null;
+  (dispatch, axios: any) => {
+    dispatch(fetchLatestArticles(limit, axios));
   };
