5143dc8383dcbc8808a434532b1e84017d514f49
dan jones <dannyjones360@gmail.com>
2018-10-08 16:38:00 +0100

    Add SocialLinks component

---
+++ src/components/Article/SocialLinks/index.js
@@ -0,0 +1,54 @@
+/* @flow */
+/* eslint-disable import/no-named-as-default, react/no-unknown-property */
+
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+export class SocialLinks extends PureComponent {
+  render() {
+    const { article } = this.props;
+    console.log(article);
+
+    if (article.socialHandles) {
+      let soundcloud = null;
+      let facebook = null;
+      let twitter = null;
+      let instagram = null;
+
+      if (article.socialHandles.soundcloud) {
+        soundcloud = <a className="db  pv2  link  f6  black  t-body" href={`https://soundcloud.com/${article.socialHandles.soundcloud}`} rel="noopener noreferrer" target="_blank">- soundcloud/{article.socialHandles.soundcloud}</a>;
+      }
+      if (article.socialHandles.facebook) {
+        facebook = <a className="db  pv2  link  f6  black  t-body" href={`https://facebook.com/${article.socialHandles.facebook}`} rel="noopener noreferrer" target="_blank">f- acebook/{article.socialHandles.facebook}</a>;
+      }
+      if (article.socialHandles.twitter) {
+        twitter = <a className="db  pv2  link  f6  black  t-body" href={`https://twitter.com/${article.socialHandles.twitter}`} rel="noopener noreferrer" target="_blank">- twitter/{article.socialHandles.twitter}</a>;
+      }
+      if (article.socialHandles.instagram) {
+        instagram = <a className="db  pv2  link  f6  black  t-body" href={`https://instagram.com/${article.socialHandles.instagram}`} rel="noopener noreferrer" target="_blank">- instagram/{article.socialHandles.instagram}</a>;
+      }
+
+
+      return (
+        <div className="pt3  ph3  bt  bc-grey  mt3">
+          {soundcloud}
+          {facebook}
+          {twitter}
+          {instagram}
+        </div>
+      );
+    }
+
+    return false;
+  }
+}
+
+SocialLinks.propTypes = {
+  article: PropTypes.shape(),
+};
+
+SocialLinks.defaultProps = {
+  article: {},
+};
+
+export default SocialLinks;
