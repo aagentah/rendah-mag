e5b56be20f580289a0261898110cbfecb0bc48c2
dan jones <dannyjones360@gmail.com>
2018-11-30 13:39:33 +0000

    Add subscribe page

---
+++ src/containers/Pages/Subscribe/index.js
@@ -0,0 +1,53 @@
+/* eslint-disable import/no-named-as-default */
+
+import React, { PureComponent } from 'react';
+import Helmet from 'react-helmet';
+
+import Hero from '../../../components/Hero';
+import SubscribeBanner from '../../../components/SubscribeBanner';
+import LatestArticles from '../../../containers/Fragments/LatestArticles';
+import CategoryGrid from '../../../components/CategoryGrid';
+
+export class Subscribe extends PureComponent {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+  componentDidUpdate() {
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    const title = 'Subscribe';
+    const desc = 'The latest news & releases, straight to your inbox.';
+    const canonical = 'https://www.rendahmag.com/subscribe';
+
+    return (
+      <main className="page-fade-in">
+        <Helmet>
+          <title>{title}</title>
+          <meta name="description" content={desc} />
+          <link rel="canonical" href={canonical} />
+        </Helmet>
+
+        <Hero type="h1" title={title} styles="t-title  ttu  f3  bold  dark-grey" padding="pb4" />
+
+        <div className="container-medium  center  pt4  pb4">
+          <div className="flex  flex-wrap">
+            <div className="col-24">
+              <p className="t-body  dark-grey  f6  tac  mw6  db  center  pb2">{desc}</p>
+            </div>
+          </div>
+
+          <div className="center  w-100  mw6  ph2  ph5-sm  pv3">
+            <SubscribeBanner />
+          </div>
+        </div>
+
+        <LatestArticles limit={4} type="grid" padding="pb4" />
+        <CategoryGrid padding="" />
+      </main>
+    );
+  }
+}
+
+export default Subscribe;
