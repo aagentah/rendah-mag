e0dc2212c0996cd7fb25b2bfecbf43886ef35e5a
dan jones <dannyjones360@gmail.com>
2018-12-18 11:10:11 +0000

    Split AuthorList into multiple components and add alias check for parenthesis

---
+++ src/components/AuthorList/Item/index.js
@@ -0,0 +1,43 @@
+/* eslint-disable import/no-named-as-default */
+
+import React, { PureComponent } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import AnimatedImage from '../../Elements/AnimatedImage';
+
+export class AuthorList extends PureComponent {
+  render() {
+    const { author } = this.props;
+    const { alias } = author;
+
+    return (
+      <div className="col-24  col-12-sm  col-6-md  ph3  pb3">
+        <figure className="rel  pb3  link  w-100">
+          <Link title={author.slug} to={`/author/${author.slug}`} className="link  db  h4  w4  shadow2  center  br-100">
+            <AnimatedImage
+              lazy
+              src={author.img}
+              alt={author.name}
+              styles="fade-in-zoom-in  h4  w4  center  br-100"
+            />
+          </Link>
+        </figure>
+        <Link title={author.slug} to={`/author/${author.slug}`} className="t-title  black  f6  link  db  pt1  pb3  tac  cp">
+          <span className="db">{author.name}</span>
+          <span className="db  t8  grey  pt1">{(alias) ? `(${alias})` : ''}</span>
+        </Link>
+      </div>
+    );
+  }
+}
+
+AuthorList.propTypes = {
+  author: PropTypes.shape(),
+};
+
+AuthorList.defaultProps = {
+  author: {},
+};
+
+export default AuthorList;
--- src/components/AuthorList/index.js
+++ src/components/AuthorList/index.js
@@ -1,10 +1,9 @@
 /* eslint-disable import/no-named-as-default */
 
 import React, { PureComponent } from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-import AnimatedImage from '../Elements/AnimatedImage';
+import Item from './Item';
 
 export class AuthorList extends PureComponent {
   render() {
@@ -14,22 +13,7 @@ export class AuthorList extends PureComponent {
       <div className={`container-medium  center  ${padding}`}>
         <div className="flex  flex-wrap">
           {list.map(author => (
-            <div key={author.name} className="col-24  col-12-sm  col-6-md  ph3  pb3">
-              <figure className="rel  pb3  link  w-100">
-                <Link title={author.slug} to={`/author/${author.slug}`} className="link  db  h4  w4  shadow2  center  br-100">
-                  <AnimatedImage
-                    lazy
-                    src={author.img}
-                    alt={author.name}
-                    styles="fade-in-zoom-in  h4  w4  center  br-100"
-                  />
-                </Link>
-              </figure>
-              <Link title={author.slug} to={`/author/${author.slug}`} className="t-title  black  f6  link  db  pt1  pb3  tac  cp">
-                <span className="db">{author.name}</span>
-                <span className="db  t8  grey  pt1">({author.alias})</span>
-              </Link>
-            </div>
+            <Item key={author.name} author={author} />
           ))}
         </div>
       </div>
