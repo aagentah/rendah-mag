2e164b4d771fc9477f6202216d90d63ab404bc6b
Danny Jones <dannyjones360@gmail.com>
2019-06-27 13:42:40 +0100

    Add SEO and minor styles

--- src/components/Product/Seo/index.js
+++ src/components/Product/Seo/index.js
@@ -7,30 +7,33 @@ import PropTypes from 'prop-types';
 
 export class Seo extends PureComponent {
   render() {
-    const { title, slug, description, img, created, teamMember } = this.props;
+    const { title, slug, description, img, price } = this.props;
 
     return (
       <Helmet>
         <title>{title}</title>
         <meta name="description" content={description} />
-        <link rel="canonical" href={`https://www.rendahmag.com/article/${slug}`} />
+        <link rel="canonical" href={`https://www.rendahmag.com/product/${slug}`} />
 
         {/* Twitter  */}
+        <meta name="twitter:card" content="product" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={img} />
+        <meta name="twitter:data1" content={price} />
+        <meta name="twitter:label1" content="Price" />
 
         {/* Open Graph data */}
         <meta property="og:title" content={title} />
-        <meta property="og:url" content={`https://www.rendahmag.com/article/${slug}`} />
+        <meta property="og:url" content={`https://www.rendahmag.com/product/${slug}`} />
         <meta property="og:image" content={img} />
         <meta property="og:image:width" content="1080" />
         <meta property="og:image:height" content="1080" />
+        <meta property="og:type" content="og:product" />
         <meta property="og:description" content={description} />
-        <meta property="article:published_time" content={created} />
-        <meta property="article:modified_time" content={created} />
-        <meta property="article:section" content="Article" />
-        <meta property="article:teamMember" content={teamMember} />
+        <meta property="product:plural_title" content={title} />
+        <meta property="product:price:amount" content={price} />
+        <meta property="product:price:currency" content="GBP" />
       </Helmet>
     );
   }
@@ -41,8 +44,7 @@ Seo.propTypes = {
   slug: PropTypes.string,
   description: PropTypes.string,
   img: PropTypes.string,
-  created: PropTypes.string,
-  teamMember: PropTypes.string,
+  price: PropTypes.string,
 };
 
 Seo.defaultProps = {
@@ -50,8 +52,7 @@ Seo.defaultProps = {
   slug: '',
   description: '',
   img: '',
-  created: '',
-  teamMember: '',
+  price: '',
 };
 
 export default Seo;
