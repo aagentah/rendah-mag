34891fade64576d230226625a56c4922022e7df4
Danny Jones <dannyjones360@gmail.com>
2019-07-05 16:30:12 +0100

    Further progress to store styles

--- src/containers/Pages/Product/index.js
+++ src/containers/Pages/Product/index.js
@@ -20,11 +20,23 @@ export class Article extends PureComponent {
       <main className="page-fade-in">
         <div className="container-medium  center  pv4">
           <div className="flex  flex-wrap">
-            <div className="col-24  col-6-md">
-              <span className="t-title  black  f6  bold  db">Collections</span>
-              <Collections range={[1, 24]} type="grid" padding="pv2  pl2  pr4" />
-              <span className="t-title  black  f6  bold  db">Categories</span>
-              <Categories range={[1, 24]} type="grid" padding="pv2  pl2  pr4" />
+            <div className="col-24  col-6-md  ph2  ph0-md">
+              <span className="t-title  black  f6  bold  dn  db-md">Collections</span>
+              <Collections
+                {...this.props}
+                range={[1, 24]}
+                name="Collections"
+                type="grid"
+                padding="pv2  pl2  pr4"
+              />
+              <span className="t-title  black  f6  bold  dn  db-md">Categories</span>
+              <Categories
+                {...this.props}
+                range={[1, 24]}
+                name="Categories"
+                type="grid"
+                padding="pv2  pl2  pr4"
+              />
             </div>
             <div className="col-24  col-18-md">
               <ProductInfo match={this.props.match} {...this.props} />
