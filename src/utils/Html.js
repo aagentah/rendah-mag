2f5751e814c83da9aae44743b9db92e384c6f20c
dan jones <dannyjones360@gmail.com>
2019-09-26 19:26:37 +0100

    'Minors'

--- src/utils/Html.js
+++ src/utils/Html.js
@@ -26,7 +26,7 @@ const Html = ({ store, htmlContent, noServerRender }: Props): Element<'html'> =>
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
         {head.meta.toComponent()}
         {head.title.toComponent()}
