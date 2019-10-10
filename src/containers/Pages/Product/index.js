a13be44dc34feef348e60a0dfcc65ba329a42d1f
danjonesdev <dannyjones360@gmail.com>
2019-10-10 13:42:55 +0100

    Store launch

--- src/containers/Pages/Product/index.js
+++ src/containers/Pages/Product/index.js
@@ -2,6 +2,7 @@
 
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+
 import Collections from '../../../containers/Fragments/Store/Collections';
 import Categories from '../../../containers/Fragments/Store/Categories';
 import ProductInfo from '../../../containers/Fragments/Store/ProductInfo';
