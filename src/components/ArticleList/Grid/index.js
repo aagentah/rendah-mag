6b411f505ee9cad2f98d448d581089c84f0a3d36
dan jones <dannyjones360@gmail.com>
2019-10-30 00:18:22 +0000

    Added grid card imports

--- src/components/ArticleList/Grid/index.js
+++ src/components/ArticleList/Grid/index.js
@@ -1,69 +1,100 @@
 /* eslint-disable import/no-named-as-default */
 
 import React, { PureComponent } from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import trim from 'lodash/trim';
+import { Card, Image, Heading, Copy, Button } from 'rendah-pattern-library';
 
-import { convertDate, toUrlCase } from '../../../functions';
-import AnimatedImage from '../../Elements/AnimatedImage';
+import { convertDate } from '../../../functions';
 
 export class ArticleListGrid extends PureComponent {
   date = date => convertDate(date);
 
   render() {
-    const { list, padding, type } = this.props;
-    let containerClass;
-    let imageHeight;
+    const { list, padding } = this.props;
 
-    if (type === 'featured') {
-      containerClass = 'col-24  col-12-sm';
-      imageHeight = 'h7  h11-sm';
-    }
-    if (type === 'grid') {
-      containerClass = 'col-24  col-12-sm  col-6-md';
-      imageHeight = 'h7  h7-sm';
-    }
+    const renderCard = (article) => {
+      console.log('article', article);
+
+      const withLinkProps = {
+        type: 'internal',
+        url: `/article/${article.slug}`,
+        target: '_top',
+      };
+
+      const cardImage = (
+        <Image
+          src={article.img}
+          placeholder={`${article.img}?w=100`}
+          alt={article.title}
+          figcaption={null}
+          height={250}
+          onClick={null}
+          withLinkProps={withLinkProps}
+        />
+      );
+
+      const cardHeading = (
+        <Heading
+          htmlEntity={'h2'}
+          text={article.title}
+          color={'black'}
+          size={'small'}
+          truncate={2}
+          withLinkProps={withLinkProps}
+        />
+      );
+
+      const cardCopy = (
+        <Copy
+          text={article.description}
+          color={'black'}
+          size={'medium'}
+          truncate={2}
+        />
+      );
+
+      const cardButton = (
+        <Button
+          /* Options */
+          type={'secondary'}
+          size={'medium'}
+          text={'Read more'}
+          color={'black'}
+          fluid={false}
+          icon={'arrow-right'}
+          iconFloat={null}
+          inverted={false}
+          loading={false}
+          disabled={false}
+          onClick={null}
+          withLinkProps={withLinkProps}
+        />
+      );
+
+      return (
+        <Card
+          /* Options */
+          type={'block'}
+          price={this.date(article.created)}
+          discountPrice={null}
+          onClick={null}
+          /* Children */
+          image={cardImage}
+          title={cardHeading}
+          description={cardCopy}
+          button={cardButton}
+        />
+      );
+    };
+
+    console.log('list', list);
 
     return (
       <div className={`container-medium  center  ${padding}`}>
         <div className="flex  flex-wrap">
           {list.map(article => (
-            <div key={article.title} className={`${containerClass}  pa3`}>
-              <article>
-                <figure>
-                  <Link
-                    className="db  shadow2"
-                    title={article.slug}
-                    to={`/article/${article.slug}`}
-                  >
-                    <AnimatedImage
-                      lazy
-                      src={article.img}
-                      alt={article.title}
-                      styles={`fade-in-zoom-in  ${imageHeight}  w-100`}
-                    />
-                  </Link>
-                </figure>
-
-                <div className="pv2  mt2">
-                  <span className="t-body  grey  f6">{this.date(article.created)} | </span>
-                  <Link to={`/team/${toUrlCase(trim(article.teamMember))}`} className="link">
-                    <span className="t-body  grey  f6  cp  link">{article.teamMember}</span>
-                  </Link>
-                </div>
-
-                <div>
-                  <Link to={`/article/${article.slug}`} className="t-body  db  link  pb2">
-                    <p className="t-title  lh-copy  black  f5  cp  over-hidden  link  grid-card__title">
-                      {article.title}
-                    </p>
-                  </Link>
-                  <p className="t-body  lh-copy  grey  f6  over-hidden  mt1  grid-card__desc">
-                    {article.description}
-                  </p>
-                </div>
-              </article>
+            <div key={article.title} className="col-24  col-6-sm  pa3">
+              {renderCard(article)}
             </div>
           ))}
         </div>
@@ -74,13 +105,11 @@ export class ArticleListGrid extends PureComponent {
 
 ArticleListGrid.propTypes = {
   list: PropTypes.array, // eslint-disable-line react/forbid-prop-types
-  type: PropTypes.string,
   padding: PropTypes.string,
 };
 
 ArticleListGrid.defaultProps = {
   list: [],
-  type: '',
   padding: '',
 };
 
