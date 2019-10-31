a164b23e07a2d43991deed5d93083f268289c012
danjonesdev <dannyjones360@gmail.com>
2019-10-31 14:43:23 +0000

    Minors

--- src/containers/Pages/Home/index.js
+++ src/containers/Pages/Home/index.js
@@ -3,6 +3,7 @@
 import React, { PureComponent } from 'react';
 import Helmet from 'react-helmet';
 import { Link } from 'react-router-dom';
+import { Hero, Image } from 'rendah-pattern-library';
 
 import LatestArticles from '../../../containers/Fragments/Blog/LatestArticles';
 import CategoryGrid from '../../../components/CategoryGrid';
@@ -20,6 +21,34 @@ export class Home extends PureComponent {
     const desc = 'Beats, Halftime & Future Bass Magazine focused on the latest news & releases.';
     const canonical = 'https://www.rendahmag.com/';
 
+    const heroImageDesktop = (
+      <Image
+        /* Options */
+        src={'https://res.cloudinary.com/dzz8ji5lj/image/upload/v1569520981/shop/subscribe-banner-desktop_2.png'}
+        placeholder={null}
+        alt={'Rendah Mag Issue: 001'}
+        figcaption={null}
+        height={null}
+        onClick={null}
+        /* Children */
+        withLinkProps={null}
+      />
+    );
+
+    const heroImageMobile = (
+      <Image
+        /* Options */
+        src={'https://res.cloudinary.com/dzz8ji5lj/image/upload/v1569520983/shop/subscribe-banner-mobile_1.png'}
+        placeholder={null}
+        alt={'Rendah Mag Issue: 001'}
+        figcaption={null}
+        height={null}
+        onClick={null}
+        /* Children */
+        withLinkProps={null}
+      />
+    );
+
     return (
       <main className="page-fade-in">
         <Helmet>
@@ -31,32 +60,32 @@ export class Home extends PureComponent {
         <h1 className="dn">Home</h1>
 
         <div className="container-medium  center  pt4">
-          <div className="flex  flex-wrap">
-            <div className="w-100  pb2  pb0-md">
-              <Link title="Rendah Mag Issue: 001" to="/product/rendah-mag-issue-001" className="w-100  link">
-                <img className="w-100  shadow2  br2  dn  db-md" src="https://res.cloudinary.com/dzz8ji5lj/image/upload/v1569520981/shop/subscribe-banner-desktop_2.png" alt="Subscribe Banner" />
-              </Link>
-              <Link title="Rendah Mag Issue: 001" to="/product/rendah-mag-issue-001" className="w-100  link">
-                <img className="w-100  shadow2  br2  db  dn-md" src="https://res.cloudinary.com/dzz8ji5lj/image/upload/v1569520983/shop/subscribe-banner-mobile_1.png" alt="Subscribe Banner" />
-              </Link>
-            </div>
+          <div className="dn  db-md">
+            <Hero
+              /* Options */
+              color={'black'}
+              height={null}
+              /* Children */
+              image={heroImageDesktop}
+              title={null}
+              description={null}
+              button={null}
+            />
+          </div>
+
+          <div className="db  dn-md">
+            <Hero
+              /* Options */
+              color={'black'}
+              height={null}
+              /* Children */
+              image={heroImageMobile}
+              title={null}
+              description={null}
+              button={null}
+            />
           </div>
         </div>
-
-        {/*
-          <div className="container-medium  center  pt3  pt4-md">
-            <div className="flex  flex-wrap">
-              <div className="w-100  pb3  pb4-md">
-                <Link title="Subscribe" to="/product/rendah-mag-issue-001" className="w-100  link">
-                  <img className="w-100  shadow2  dn  db-md" src="https://res.cloudinary.com/dzz8ji5lj/image/upload/v1569520981/shop/subscribe-banner-desktop_2.png" alt="Subscribe Banner" />
-                </Link>
-                <Link title="Subscribe" to="/product/rendah-mag-issue-001" className="w-100  link">
-                  <img className="w-100  shadow2  db  dn-md" src="https://res.cloudinary.com/dzz8ji5lj/image/upload/v1569520983/shop/subscribe-banner-mobile_1.png" alt="Subscribe Banner" />
-                </Link>
-              </div>
-            </div>
-          </div>
-        */}
 
         <div className="container-medium  center  mt2  pt4  pb4">
           <div className="col-24  tac">
