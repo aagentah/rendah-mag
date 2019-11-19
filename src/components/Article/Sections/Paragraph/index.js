0b4bb62f240f0610bac761e8687a3b74bf2e6417
danjonesdev <dannyjones360@gmail.com>
2019-11-19 15:30:59 +0000

    Add inline line to article sections

--- src/components/Article/Sections/Paragraph/index.js
+++ src/components/Article/Sections/Paragraph/index.js
@@ -6,8 +6,19 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Paragraph extends Component {
-  renderChildren = (child, i) => {
+  renderChildren = (child, markDefs, i) => {
     if (child.marks) {
+      // If has inline URL link
+      if (markDefs.length) {
+        if (child.marks[0] === markDefs[0]._key) {
+          return (
+            <a key={i} target="_blank" href={markDefs[0].url} rel="noopener noreferrer" className="di  underline  rendah-red">
+              {child.text}
+            </a>
+          );
+        }
+      }
+
       if (child.marks.includes('stong') && child.marks.includes('em')) {
         return (
           <strong key={i} className="di">
@@ -35,13 +46,13 @@ class Paragraph extends Component {
   };
 
   render() {
-    const { text } = this.props;
+    const { text, markDefs } = this.props;
 
     if (text[0].text) {
       return (
         <React.Fragment>
           <p className="db  t-body  lh-copy  f6  dark-grey  taj  pv3">
-            {text.map((child, i) => this.renderChildren(child, i))}
+            {text.map((child, i) => this.renderChildren(child, markDefs, i))}
           </p>
         </React.Fragment>
       );
@@ -52,10 +63,12 @@ class Paragraph extends Component {
 
 Paragraph.propTypes = {
   text: PropTypes.array,
+  markDefs: PropTypes.array,
 };
 
 Paragraph.defaultProps = {
   text: [],
+  markDefs: [],
 };
 
 export default Paragraph;
