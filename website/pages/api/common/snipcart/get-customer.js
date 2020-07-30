8503af618196681b9636729924765f14868ef3f8
danjonesdev <dannyjones360@gmail.com>
2020-07-30 22:55:20 +0100

    Cleanups

--- website/pages/api/common/snipcart/get-customer.js
+++ website/pages/api/common/snipcart/get-customer.js
@@ -1,94 +1,86 @@
-import request from 'request';
+import fetch from 'isomorphic-unfetch';
 import find from 'lodash/find';
 
 export default async (req, res) => {
   const { email } = req.body;
   // const email = 'Goldtoofgarms@hotmail.com';
 
-  function fetchCustomers() {
-    return new Promise((resolve) => {
-      request(
-        {
-          url: 'http://app.snipcart.com/api/customers',
-          auth: {
-            user: process.env.SNIPCART_SECRET_KEY,
-          },
-          method: 'GET',
-          json: true,
+  const secret = Buffer.from(process.env.SNIPCART_SECRET_KEY).toString(
+    'base64'
+  );
+
+  try {
+    const fetchCustomers = await fetch(
+      'http://app.snipcart.com/api/customers',
+      {
+        headers: {
+          Authorization: `Basic ${secret}`,
+          Accept: 'application/json',
         },
-        (error, response) => {
-          resolve(response.body);
-        }
-      );
-    });
-  }
+        method: 'GET',
+      }
+    );
 
-  function fetchCustomerOrdersById(id) {
-    return new Promise((resolve) => {
-      request(
-        {
-          url: `http://app.snipcart.com/api/customers/${id}/orders`,
-          auth: {
-            user: process.env.SNIPCART_SECRET_KEY,
-          },
-          method: 'GET',
-          json: true,
+    const fetchCustomerOrdersById = async (id) =>
+      await fetch(`http://app.snipcart.com/api/customers/${id}/orders`, {
+        headers: {
+          Authorization: `Basic ${secret}`,
+          Accept: 'application/json',
         },
-        (error, response) => {
-          resolve(response.body);
-        }
-      );
-    });
-  }
+        method: 'GET',
+      });
 
-  function fetchCustomerOrderByToken(token) {
-    return new Promise((resolve) => {
-      request(
-        {
-          url: `http://app.snipcart.com/api/orders/${token}`,
-          auth: {
-            user: process.env.SNIPCART_SECRET_KEY,
-          },
-          method: 'GET',
-          json: true,
+    const fetchCustomerOrderByToken = async (token) =>
+      await fetch(`http://app.snipcart.com/api/orders/${token}`, {
+        headers: {
+          Authorization: `Basic ${secret}`,
+          Accept: 'application/json',
         },
-        (error, response) => {
-          resolve(response.body);
-        }
-      );
-    });
-  }
+        method: 'GET',
+      });
 
-  async function action() {
-    const customers = await fetchCustomers();
+    const action = async () => {
+      // Fetch all customers
+      if (fetchCustomers.status >= 400) return false;
+      const customers = await fetchCustomers.json();
 
-    if (customers.items) {
-      const customerwithEmail = find(customers.items, { email: email });
+      // Find customer based on email
+      if (!customers.items) return false;
+      const customer = find(customers.items, { email: email });
+      if (!customer?.id) return false;
 
-      if (customerwithEmail?.id) {
-        const customerOrders = await fetchCustomerOrdersById(
-          customerwithEmail.id
+      // Fetch customer's orders based on Id
+      const customerOrdersResponse = await fetchCustomerOrdersById(customer.id);
+      if (fetchCustomers.status >= 400) return false;
+      const customerOrders = await customerOrdersResponse.json();
+      if (!customerOrders) return false;
+
+      // Fetch each order's details
+      const ordersByToken = [];
+
+      for (let i = 0; i < customerOrders.length; i++) {
+        const orderResponse = await fetchCustomerOrderByToken(
+          customerOrders[i].token
         );
 
-        if (customerOrders) {
-          const ordersByToken = [];
-
-          for (let i = 0; i < customerOrders.length; i++) {
-            const order = await fetchCustomerOrderByToken(
-              customerOrders[i].token
-            );
-            ordersByToken.push(order);
-          }
-
-          return res.status(200).json(ordersByToken);
-        }
+        if (orderResponse.status >= 400) continue;
+        const order = await orderResponse.json();
+        ordersByToken.push(order);
       }
 
+      return ordersByToken;
+    };
+
+    const response = await action();
+
+    // Handle response
+    if (response) {
+      return res.status(200).json(response);
+    } else {
       return res.status(400).json([]);
     }
-
-    return res.status(400).json([]);
+  } catch (error) {
+    // Catch errors
+    return res.status(500).json([]);
   }
-
-  return action();
 };
