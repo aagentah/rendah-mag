8e95a6003ef1316cb19c5fae7d2bdc570a1920bb
danjonesdev <dannyjones360@gmail.com>
2020-08-05 00:31:19 +0100

    Ammends to CMS

--- cms/schemas/schema.js
+++ cms/schemas/schema.js
@@ -1,30 +1,42 @@
-import createSchema from 'part:@sanity/base/schema-creator'
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import createSchema from "part:@sanity/base/schema-creator";
+import schemaTypes from "all:part:@sanity/base/schema-type";
 
-import blockContent from './blockContent'
+import blockContent from "./blockContent";
 
-import category from './documents/category'
-import post from './documents/post'
-import author from './documents/author'
-import cypher from './documents/cypher'
-import user from './documents/user'
+// Blog
+import post from "./documents/post";
+import author from "./documents/author";
+import blogCategory from "./documents/blogCategory";
 
-import siteSettings from './objects/siteSettings'
+// Store
+import storeCategory from "./documents/storeCategory";
+import storeCollection from "./documents/storeCollection";
+import storeItem from "./documents/storeItem";
+import storeVariant from "./documents/storeVariant";
 
+// Cypher
+import cypher from "./documents/cypher";
+
+// User
+import user from "./documents/user";
+
+import siteSettings from "./objects/siteSettings";
 
 export default createSchema({
-  name: 'default',
-  types: schemaTypes.concat(
-    [
-      blockContent,
-      //
-      post,
-      author,
-      cypher,
-      category,
-      user,
-      //
-      siteSettings
-    ]
-  )
-})
+  name: "default",
+  types: schemaTypes.concat([
+    blockContent,
+    //
+    post,
+    author,
+    blogCategory,
+    storeCategory,
+    storeCollection,
+    storeItem,
+    storeVariant,
+    cypher,
+    user,
+    //
+    siteSettings,
+  ]),
+});
