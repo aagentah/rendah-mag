845e4a8ef6fb3837782c0f5bf0bf1278fa960d17
danjonesdev <dannyjones360@gmail.com>
2020-08-20 22:08:43 +0100

    Minors

---
+++ website/components/article/s/image/index.js
@@ -0,0 +1,25 @@
+import { Image } from 'next-pattern-library';
+
+import { imageBuilder } from '../../../../lib/sanity/requests';
+
+export default function ImageSection({ section }) {
+  return (
+    <figure>
+      <Image
+        /* Options */
+        src={imageBuilder.image(section.asset).url()}
+        placeholder={imageBuilder
+          .image(section.asset)
+          .height(25)
+          .width(25)
+          .url()}
+        alt="This is the alt text."
+        figcaption={null}
+        height={null}
+        onClick={null}
+        /* Children */
+        withLinkProps={null}
+      />
+    </figure>
+  );
+}
