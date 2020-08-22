257bb981c1e0c12e32d43922fc702bd510d81edb
danjonesdev <dannyjones360@gmail.com>
2020-08-22 03:12:24 +0100

    Add image compression module to CMS

--- website/pages/api/cors.js
+++ website/pages/api/cors.js
@@ -14,17 +14,6 @@ export default async function handler(req, res) {
   // Run cors
   await cors(req, res);
 
-  const blob = req.body.blob;
-
-  client.assets
-    .upload('image', blob, {
-      contentType: 'image/png',
-      filename: 'someText.png',
-    })
-    .then((document) => {
-      res.json({ message: `The image was uploaded!, ${document}` });
-    })
-    .catch((error) => {
-      res.json({ message: `Upload failed:, ${error.message}` });
-    });
+  // Rest of the API logic
+  res.json({ message: 'Hello Everyone!' });
 }
