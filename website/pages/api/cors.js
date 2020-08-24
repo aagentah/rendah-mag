de3233a5ac480fae5f41205253ef189edf641194
danjonesdev <dannyjones360@gmail.com>
2020-08-24 10:16:44 +0100

    Add tinify to api/cors

--- website/pages/api/cors.js
+++ website/pages/api/cors.js
@@ -1,4 +1,6 @@
 import Cors from 'cors';
+import tinify from 'tinify';
+
 import initMiddleware from '../../lib/init-middleware';
 
 // Initialize the cors middleware
@@ -13,6 +15,14 @@ const cors = initMiddleware(
 export default async function handler(req, res) {
   // Run cors
   await cors(req, res);
+
+  tinify.key = '2hFnFm74VZHK9P5bq0P4r8kMdNY3qlP0';
+
+  const { imageUrl } = req.body;
+  const source = tinify.fromUrl(imageUrl);
+
+  source.toFile('optimized.jpg');
+  console.log('source', source);
 
   // Rest of the API logic
   res.json({ message: 'Hello Everyone!' });
