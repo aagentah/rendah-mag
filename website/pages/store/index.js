6abc1a6b87630c55d551237e4c64fdd47b585b95
danjonesdev <dannyjones360@gmail.com>
2020-08-28 17:53:16 +0100

    Minors

--- website/pages/store/index.js
+++ website/pages/store/index.js
@@ -7,6 +7,7 @@ import { Heading } from 'next-pattern-library';
 import Layout from '../../components/layout';
 import Container from '../../components/layout/container';
 import CardProduct from '../../components/card/product';
+import HeroStore from '../../components/hero/store';
 
 import { getSiteConfig, getAllProducts } from '../../lib/sanity/requests';
 
@@ -30,12 +31,10 @@ export default function Post({ siteConfig, allProducts }) {
     collectionGroups.push(collectionWithProducts);
   }
 
-  console.log('collectionGroups', collectionGroups);
-
   return (
     <Layout
-      navOffset="top"
-      navOnWhite
+      navOffset={null}
+      navOnWhite={false}
       meta={{
         siteConfig,
         title: 'Store',
@@ -44,62 +43,66 @@ export default function Post({ siteConfig, allProducts }) {
       }}
       preview={null}
     >
-      <Container>
-        <div className="pt4  pb2">
-          <Heading
-            /* Options */
-            htmlEntity="h1"
-            text="Store"
-            color="black"
-            size="large"
-            truncate={0}
-            onClick={null}
-            /* Children */
-            withLinkProps={null}
-          />
-        </div>
+      <HeroStore />
 
-        {collectionGroups.length === 0 && (
-          <section className="pb3">
-            <h2 className="t-primary  f5  lh-title  grey  tal  pb4">
-              - No Results for &quot;
-              {params.slug}
-              &quot;
-            </h2>
-          </section>
-        )}
+      <div className="pt6">
+        <Container>
+          <div className="pb2">
+            <Heading
+              /* Options */
+              htmlEntity="h1"
+              text="Store"
+              color="black"
+              size="large"
+              truncate={0}
+              onClick={null}
+              /* Children */
+              withLinkProps={null}
+            />
+          </div>
 
-        {collectionGroups.length > 0 && (
-          <section className="pb3  pt4">
-            <div className="flex  flex-wrap">
-              {collectionGroups.map((group, i) => (
-                <div key={group.name}>
-                  <div className="flex  flex-wrap  pb3  bb  bc-black  mb3">
-                    <Heading
-                      /* Options */
-                      htmlEntity="h1"
-                      text={group.name}
-                      color="black"
-                      size="medium"
-                      truncate={0}
-                      onClick={null}
-                      /* Children */
-                      withLinkProps={null}
-                    />
+          {collectionGroups.length === 0 && (
+            <section className="pb3">
+              <h2 className="t-primary  f5  lh-title  grey  tal  pb4">
+                - No Results for &quot;
+                {params.slug}
+                &quot;
+              </h2>
+            </section>
+          )}
+
+          {collectionGroups.length > 0 && (
+            <section className="pb3  pt4">
+              <div className="flex  flex-wrap">
+                {collectionGroups.map((group, i) => (
+                  <div key={group.name}>
+                    <div className="flex  flex-wrap  pb3  bb  bc-black  mb3">
+                      <Heading
+                        /* Options */
+                        htmlEntity="h1"
+                        text={group.name}
+                        color="black"
+                        size="medium"
+                        truncate={0}
+                        onClick={null}
+                        /* Children */
+                        withLinkProps={null}
+                      />
+                    </div>
+                    <div className="flex  flex-wrap  pb3">
+                      {group.products.map((product, ii) => (
+                        <div key={product.title} className="pa3">
+                          <CardProduct i={ii} product={product} />
+                        </div>
+                      ))}
+                    </div>
                   </div>
-                  <div className="flex  flex-wrap  pb3">
-                    {group.products.map((product, ii) => (
-                      <div key={product.title} className="pa3">
-                        <CardProduct i={ii} product={product} />
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              ))}
-            </div>
-          </section>
-        )}
-      </Container>
+                ))}
+              </div>
+            </section>
+          )}
+        </Container>
+      </div>
     </Layout>
   );
 }
