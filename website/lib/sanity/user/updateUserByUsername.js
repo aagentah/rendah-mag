b0265945d9ad85f3adfaa4acd17a988d1e222991
danjonesdev <dannyjones360@gmail.com>
2020-09-02 23:49:53 +0100

    Cleanups

--- website/lib/sanity/user/updateUserByUsername.js
+++ website/lib/sanity/user/updateUserByUsername.js
@@ -2,91 +2,97 @@ import fs from 'fs';
 import crypto from 'crypto';
 import tinify from 'tinify';
 import { promisify } from 'util';
-
-const writeFile = promisify(fs.writeFile);
+import cloneDeep from 'lodash/cloneDeep';
 
 import client from '../config-write';
 import { SITE_URL } from '../../../constants';
 
+const handlePassword = (cloneFields) => {
+  if (!cloneFields?.password) return;
+  const salt = crypto.randomBytes(16).toString('hex');
+  const hash = crypto
+    .pbkdf2Sync(cloneFields.password, salt, 1000, 64, 'sha512')
+    .toString('hex');
+
+  cloneFields.salt = salt;
+  cloneFields.hash = hash;
+
+  delete cloneFields.password;
+  return cloneFields;
+};
+
+const handleAvatar = async (cloneFields, user) => {
+  tinify.key = process.env.TINIFY_KEY;
+
+  const writeFile = promisify(fs.writeFile);
+  const image64 = cloneFields.avatar.replace(/^data:image\/png;base64,/, '');
+  await writeFile('tmp/image.png', image64, 'base64');
+  const source = tinify.fromFile('tmp/image.png');
+  const resized = source.resize({
+    method: 'cover',
+    width: 720,
+    height: 720,
+  });
+
+  // Tinify image
+  await resized.toFile('tmp/optimized.png');
+
+  const uploadCompressed = async (imageAsset) => {
+    return await client
+      .patch(user._id)
+      .set({
+        avatar: {
+          _type: 'image',
+          asset: {
+            _type: 'reference',
+            _ref: imageAsset._id,
+          },
+        },
+      })
+      .commit()
+      .then((res) => {
+        console.log(`Image was updated, ${res._id}`);
+        return res;
+      })
+      .catch((err) => {
+        console.error('Oh no, the image update failed: ', err.message);
+        return false;
+      });
+  };
+
+  // Upload compressed image to Sanity
+  await client.assets
+    .upload('image', fs.createReadStream('tmp/optimized.png'), {
+      contentType: 'image/png',
+      filename: `optimized.png`,
+    })
+    .then(async (imageAsset) => {
+      await uploadCompressed(imageAsset);
+    })
+    .catch((error) => {
+      console.error('Upload failed:', error.message);
+    });
+
+  delete cloneFields.avatar;
+  return cloneFields;
+};
+
 export async function updateUserByUsername(req, user, fields) {
   try {
+    // Clone the fields object
+    let cloneFields = cloneDeep(fields);
+
     // Handle password change
-    if (fields?.password) {
-      const salt = crypto.randomBytes(16).toString('hex');
-      const hash = crypto
-        .pbkdf2Sync(fields.password, salt, 1000, 64, 'sha512')
-        .toString('hex');
-
-      fields.salt = salt;
-      fields.hash = hash;
-      delete fields.password;
-    }
-
-    const uploadAvatar = async () => {
-      const image64 = fields.avatar.replace(/^data:image\/png;base64,/, '');
-
-      await writeFile('tmp/image.png', image64, 'base64');
-
-      const source = tinify.fromFile('tmp/image.png');
-
-      const resized = source.resize({
-        method: 'cover',
-        width: 720,
-        height: 720,
-      });
-
-      // Tinify image
-      await resized.toFile('tmp/optimized.png');
-
-      const uploadCompressed = async (imageAsset) => {
-        return await client
-          .patch(user._id)
-          .set({
-            avatar: {
-              _type: 'image',
-              asset: {
-                _type: 'reference',
-                _ref: imageAsset._id,
-              },
-            },
-          })
-          .commit()
-          .then((res) => {
-            console.log(`Image was updated, ${res._id}`);
-            return res;
-          })
-          .catch((err) => {
-            console.error('Oh no, the image update failed: ', err.message);
-            return false;
-          });
-      };
-
-      // Upload compressed image to Sanity
-      await client.assets
-        .upload('image', fs.createReadStream('tmp/optimized.png'), {
-          contentType: 'image/png',
-          filename: `optimized.png`,
-        })
-        .then(async (imageAsset) => {
-          await uploadCompressed(imageAsset);
-        })
-        .catch((error) => {
-          console.error('Upload failed:', error.message);
-        });
-
-      delete fields.avatar;
-      return;
-    };
+    handlePassword(cloneFields);
 
     // Handle image change
-    if (fields?.avatar) {
-      tinify.key = process.env.TINIFY_KEY;
-      await uploadAvatar();
-    }
+    if (cloneFields?.avatar)
+      cloneFields = await handleAvatar(cloneFields, user);
 
+    // Update user
     const data = await client
       .patch(user._id)
-      .set(fields)
+      .set(cloneFields)
       .commit()
       .then((res) => {
         console.log(`User was updated, document ID is ${res._id}`);
