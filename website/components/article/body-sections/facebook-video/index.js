4447b528abc8ec4e6cd47bd089cb8a56460b1b52
danjonesdev <dannyjones360@gmail.com>
2020-09-11 00:43:32 +0100

    Minors

--- website/components/article/body-sections/facebook-video/index.js
+++ website/components/article/body-sections/facebook-video/index.js
@@ -1,11 +1,14 @@
-import FacebookProvider, { EmbeddedVideo } from 'react-facebook';
+import { FacebookProvider, EmbeddedVideo } from 'react-facebook';
 
 export default function FacebookVideo({ url }) {
+  console.log('url', url);
   return (
-    <div className="center  tac  db  w-90  center">
-      <FacebookProvider appId="154881868603516">
-        <EmbeddedVideo href={url} />
-      </FacebookProvider>
+    <div className="tac  db  w-90  mla  mra">
+      {
+        <FacebookProvider appId="154881868603516">
+          <EmbeddedVideo href={url} />
+        </FacebookProvider>
+      }
     </div>
   );
 }
--- website/components/article/body-sections/soundcloud/index.js
+++ website/components/article/body-sections/soundcloud/index.js
@@ -2,7 +2,7 @@ import Iframe from 'react-iframe';
 
 export default function Soundcloud({ url }) {
   return (
-    <div className="w-90  db  center">
+    <div className="w-90  db  mla  mra">
       <Iframe
         url={`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/${url}&color=%23777777&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=false&visual=false`}
         width="100%"
