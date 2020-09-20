7aa77e230bc246e1408c3ef6ea8116adbe7d826f
danjonesdev <dannyjones360@gmail.com>
2020-09-20 17:55:05 +0100

    Lazy load article components

--- website/components/article/body-sections/facebook-video/index.js
+++ website/components/article/body-sections/facebook-video/index.js
@@ -1,13 +1,23 @@
 import { FacebookProvider, EmbeddedVideo } from 'react-facebook';
+import LazyLoad from 'react-lazyload';
+
+import { useApp } from '~/context-provider/app';
 
 export default function FacebookVideo({ url }) {
+  const app = useApp();
   console.log('url', url);
   return (
     <div className="tac  db  w-90  mla  mra">
       {
-        <FacebookProvider appId="154881868603516">
-          <EmbeddedVideo href={url} />
-        </FacebookProvider>
+        <LazyLoad
+          once
+          offset={150}
+          height={app.deviceType === 'mobile' ? 266 : 490}
+        >
+          <FacebookProvider appId="154881868603516">
+            <EmbeddedVideo href={url} />
+          </FacebookProvider>
+        </LazyLoad>
       }
     </div>
   );
--- website/components/article/body-sections/youtube/index.js
+++ website/components/article/body-sections/youtube/index.js
@@ -1,12 +1,15 @@
 import YouTube from 'react-youtube';
+import LazyLoad from 'react-lazyload';
 
 export default function Youtube({ videoId }) {
   return (
     <>
-      <YouTube
-        className="center  tac  db  w-90  center  h6"
-        videoId={videoId}
-      />
+      <LazyLoad once offset={150} height={360}>
+        <YouTube
+          className="center  tac  db  w-90  center  h6"
+          videoId={videoId}
+        />
+      </LazyLoad>
     </>
   );
 }
