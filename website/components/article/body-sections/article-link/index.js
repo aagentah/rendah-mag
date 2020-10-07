f3753cdc0bcbf64494a73a2a2a236efe671e3823
danjonesdev <dannyjones360@gmail.com>
2020-10-07 08:22:00 +0100

    Add button to article sections

--- website/components/article/body-sections/article-link/index.js
+++ website/components/article/body-sections/article-link/index.js
@@ -1,14 +1,32 @@
-export default function ArticleLink({ url }) {
-  // const withLinkProps = {
-  //   type: 'external',
-  //   url: url,
-  //   target: '_blank',
-  //   routerLink: null,
-  // };
+import Link from 'next/link';
 
+import { Button } from 'next-pattern-library';
+
+export default function ArticleLink({ text, url }) {
   return (
     <div className="pv3  tal">
-      <div className="col-24">button</div>
+      <Button
+        /* Options */
+        type={'primary'}
+        size={'small'}
+        text={text}
+        color={'black'}
+        fluid={false}
+        icon={null}
+        iconFloat={null}
+        inverted={false}
+        loading={false}
+        disabled={false}
+        onClick={null}
+        /* Children */
+        withLinkProps={{
+          type: 'external',
+          href: url,
+          target: '_blank',
+          routerLink: null,
+          routerLinkProps: null,
+        }}
+      />
     </div>
   );
 }
