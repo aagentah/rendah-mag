ee11c36767cd0bc85f375d81b88d22964d9b4978
danjonesdev <dannyjones360@gmail.com>
2020-10-09 00:52:34 +0100

    Re-add ga losteners

--- website/lib/gtag.js
+++ website/lib/gtag.js
@@ -4,6 +4,8 @@ export const GA_TRACKING_ID = 'UA-120300345-1'; // This is your GA Tracking ID
 export const pageview = (url) => {
   if (process.env.NODE_ENV !== 'production') return;
 
+  console.log('1 window?.gtag', window?.gtag);
+
   window?.gtag &&
     window.gtag('config', GA_TRACKING_ID, {
       page_path: url,
@@ -13,6 +15,8 @@ export const pageview = (url) => {
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }) => {
   if (process.env.NODE_ENV !== 'production') return;
+
+  console.log('2 window?.gtag', window?.gtag);
 
   window?.gtag &&
     window.gtag('event', action, {
