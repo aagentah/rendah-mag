1cb2c6c679c045ff9af8eea6667f2f455b9b525f
danjonesdev <dannyjones360@gmail.com>
2020-10-09 11:16:19 +0100

    Style youtube

--- website/components/article/body-sections/youtube/index.js
+++ website/components/article/body-sections/youtube/index.js
@@ -5,10 +5,7 @@ export default function Youtube({ videoId }) {
   return (
     <>
       <LazyLoad once offset={150} height={360}>
-        <YouTube
-          className="center  tac  db  w-90  center  h6"
-          videoId={videoId}
-        />
+        <YouTube className="db  mla  mra  w-90" videoId={videoId} />
       </LazyLoad>
     </>
   );
