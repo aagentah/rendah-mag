5f9f2732a78f92401c14c85badc7674cb6e66cd5
danjonesdev <dannyjones360@gmail.com>
2020-10-10 23:25:45 +0100

    Fetch homepage content via async function to assist FCP

--- website/lib/hooks.jsx
+++ website/lib/hooks.jsx
@@ -1,18 +1,33 @@
-import useSWR from "swr";
+import useSWR from 'swr';
 
 // export const fetcher = url => fetch(url).then(r => r.json());
 
-const fetcher = url =>
-  fetch(url)
-    .then(r => r.json())
-    .then(data => {
-      return { user: data?.user || null };
-    });
-
 export function useUser() {
-  const { data, error, mutate } = useSWR("/api/user", fetcher);
+  const fetcher = (url) =>
+    fetch(url)
+      .then((r) => r.json())
+      .then((data) => {
+        return { user: data?.user || null };
+      });
+
+  const { data, error, mutate } = useSWR('/api/user', fetcher);
   // if data is not defined, the query has not completed
   const loading = !data;
   const user = data?.user;
   return [user, { mutate, loading, error }];
+}
+
+export function useJsonData(url) {
+  const fetcher = (url) =>
+    fetch(url)
+      .then((r) => r.json())
+      .then((data) => {
+        return { data: data || null };
+      });
+
+  const { data, error } = useSWR(url, fetcher);
+  // if data is not defined, the query has not completed
+  const loading = !data;
+  console.log('data', data);
+  return [data, { loading, error }];
 }
