6055e7d40777563f78852d7e8664a6e0683d32fe
danjonesdev <dannyjones360@gmail.com>
2020-10-12 00:18:17 +0100

    Change to preact

--- website/next.config.js
+++ website/next.config.js
@@ -1,12 +1,8 @@
+const withPrefresh = require('@prefresh/next');
 const path = require('path');
 require('dotenv').config();
 
-module.exports = {
-  webpack: (config) => {
-    const configClone = config;
-    configClone.resolve.alias['~'] = path.resolve(__dirname);
-    return configClone;
-  },
+module.exports = withPrefresh({
   env: {
     SANITY_PREVIEW_SECRET: process.env.SANITY_PREVIEW_SECRET,
     SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
@@ -23,4 +19,44 @@ module.exports = {
     SITE_URL: process.env.SITE_URL,
     DOMAIN_TYPE: process.env.DOMAIN_TYPE,
   },
-};
+  webpack(config, { dev, isServer }) {
+    // Move Preact into the framework chunk instead of duplicating in routes:
+    const splitChunks = config.optimization && config.optimization.splitChunks;
+    if (splitChunks) {
+      const cacheGroups = splitChunks.cacheGroups;
+      const test = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
+      if (cacheGroups.framework) {
+        cacheGroups.preact = Object.assign({}, cacheGroups.framework, { test });
+        // if you want to merge the 2 small commons+framework chunks:
+        // cacheGroups.commons.name = 'framework';
+      }
+    }
+
+    if (isServer) {
+      // mark `preact` stuffs as external for server bundle to prevent duplicate copies of preact
+      config.externals.push(
+        /^(preact|preact-render-to-string|preact-context-provider)([\\/]|$)/
+      );
+    }
+
+    // Install webpack aliases:
+    const aliases = config.resolve.alias || (config.resolve.alias = {});
+    aliases.react = aliases['react-dom'] = 'preact/compat';
+
+    // Automatically inject Preact DevTools:
+    if (dev && !isServer) {
+      const entry = config.entry;
+      config.entry = () =>
+        entry().then((entries) => {
+          entries['main.js'] = ['preact/debug'].concat(
+            entries['main.js'] || []
+          );
+          return entries;
+        });
+    }
+
+    config.resolve.alias['~'] = path.resolve(__dirname);
+
+    return config;
+  },
+});
