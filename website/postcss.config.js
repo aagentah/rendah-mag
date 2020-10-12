45f3cf0e36b63e3a3e7d39a4fdff1cf08b9af4bb
danjonesdev <dannyjones360@gmail.com>
2020-10-12 17:45:23 +0100

    Add postcss-purgecss

--- website/postcss.config.js
+++ website/postcss.config.js
@@ -1,20 +1,34 @@
 module.exports = {
   plugins: [
-    ...(process.env.NODE_ENV === 'production'
-      ? [
-          // [
-          //   "@fullhuman/postcss-purgecss",
-          //   {
-          //     content: [
-          //       "./pages/**/*.{js,jsx,ts,tsx}",
-          //       "./components/**/*.{js,jsx,ts,tsx}"
-          //     ],
-          //     defaultExtractor: content =>
-          //       content.match(/[\w-/:]+(?<!:)/g) || []
-          //   }
-          // ]
-        ]
-      : []),
+    // ...(process.env.NODE_ENV === 'production'
+    //   ? [
+    //       [
+    //         '@fullhuman/postcss-purgecss',
+    //         {
+    //           content: [
+    //             './pages/**/*.{js,jsx,ts,tsx}',
+    //             './components/**/*.{js,jsx,ts,tsx}',
+    //             './node_modules/next-pattern-library/**/*.{js,jsx,ts,tsx}',
+    //           ],
+    //           defaultExtractor: (content) =>
+    //             content.match(/[\w-/:]+(?<!:)/g) || [],
+    //         },
+    //       ],
+    //     ]
+    //   : []),
+    ...[
+      [
+        '@fullhuman/postcss-purgecss',
+        {
+          content: [
+            './pages/**/*.{js,jsx,ts,tsx}',
+            './components/**/*.{js,jsx,ts,tsx}',
+            './node_modules/next-pattern-library/**/*.{js,jsx,ts,tsx}',
+          ],
+          defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        },
+      ],
+    ],
     'postcss-preset-env',
   ],
 };
