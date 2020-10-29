645545497589ecc91abf326ff37f43ccd7b029c9
danjonesdev <dannyjones360@gmail.com>
2020-10-29 17:12:40 +0000

    Add minors

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -1,25 +1,36 @@
-require('dotenv').config();
-const Discord = require('discord.js');
+require("dotenv").config();
+const http = require("http");
+const express = require("express");
+const app = express();
+const Discord = require("discord.js");
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 
-bot.login(TOKEN);
+app.get("/", (request, response) => {
+  console.log("bot", bot);
+  bot.login(TOKEN);
 
-bot.on('ready', () => {
-  console.info(`Logged in as ${bot.user.tag}!`);
-});
+  bot.on("ready", () => {
+    console.info(`Logged in as ${bot.user.tag}!`);
+  });
 
-bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
-
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
+  bot.on("message", (msg) => {
+    if (msg.content === "ping") {
+      msg.reply("pong");
+      msg.channel.send("pong");
+    } else if (msg.content.startsWith("!kick")) {
+      if (msg.mentions.users.size) {
+        const taggedUser = msg.mentions.users.first();
+        msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+      } else {
+        msg.reply("Please tag a valid user!");
+      }
     }
-  }
+  });
 });
+
+app.listen(process.env.PORT);
+
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
