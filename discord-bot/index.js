2e4b17b8d1fc85910a06724cee151e2a2a65ecc0
danjonesdev <dannyjones360@gmail.com>
2020-11-02 23:02:44 +0000

    Minors

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -1,57 +1,44 @@
-// Discord.js bot
-const Discord = require("discord.js");
-const bot = new Discord.Client({
-  partials: ["MESSAGE", "CHANNEL", "REACTION", "USER"],
+// Discord.js client
+const { Client } = require("discord.js");
+const client = new Client({
+  partials: ["USER", "GUILD_MEMBER", "MESSAGE", "CHANNEL", "REACTION"],
 });
 
-bot.on("ready", () => {
-  bot.user.setActivity("Rendah Cyphers", { type: "LISTENING" });
+const guildId = "590892540610347008";
+
+const channelIds = {
+  pickYourRole: "771414803600244736",
+};
+
+const messageIds = {
+  pickYourRole: "771521982868619305",
+};
+
+const emojis = {
+  heart: {
+    symbol: "",
+    id: "771521982868619305",
+  },
+};
+
+client.on("ready", async () => {
+  client.user.setActivity("Rendah Cyphers", { type: "LISTENING" });
+
+  const m = await client.guilds
+    .get(guildId)
+    .channels.get(channelIds.pickYourRole)
+    .fetchMessages();
+
+  // client.channels.cache
+  //   .get("771414803600244736")
+  //   .messages.fetch({ limit: 1 })
+  //   .then((messages) => {
+  //     var lastMessage = messages.first();
+  //     console.log("lastMessage", lastMessage);
+  //   });
 });
 
-// bot.on("messageReactionAdd", async (reaction, user) => {
-//   console.log("reaction", reaction);
-//   console.log("user", user);
-//   // When we receive a reaction we check if the reaction is partial or not
-//   // if (reaction.partial) {
-//   // 	// If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
-//   // 	try {
-//   // 		await reaction.fetch();
-//   // 	} catch (error) {
-//   // 		console.error('Something went wrong when fetching the message: ', error);
-//   // 		// Return as `reaction.message.author` may be undefined/null
-//   // 		return;
-//   // 	}
-//   // }
-//   // Now the message has been cached and is fully available
-//   console.log(
-//     `${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`
-//   );
-//   // The reaction is now also fully available and the properties will be reflected accurately:
-//   console.log(
-//     `${reaction.count} user(s) have given the same reaction to this message!`
-//   );
-// });
-
-bot.on("message", (msg) => {
-  const channel = msg.channel;
-  const channelId = channel.id;
-  const guild = channel.guild;
-  const member = guild.members.get(msg.member.user.id);
-  const roles = guild.roles;
-
-  const channelIds = {
-    pickYourRole: "771414803600244736",
-  };
-
-  // Roles
-  const roleArtist = roles.find((role) => role.name === "artist");
-
-  //
-  if (channelId === channelIds.pickYourRole) {
-    if (msg.content === "😄") {
-      member.addRole(roleArtist);
-    }
-  }
+client.on("message", (msg) => {
   // if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
   // const command = msg.content.split(" ")[0].substr(process.env.PREFIX.length);
   // const args = msg.content.split(" ").slice(1).join(" ");
@@ -59,42 +46,41 @@ bot.on("message", (msg) => {
   //   return msg.channel.send("https://git.io/d.js-heroku");
   // else if (command === "invite") return msg.channel.send(process.env.INVITE);
 
+  const channel = msg.channel;
+  const channelId = channel.id;
+  const guild = channel.guild;
+  const member = guild.members.get(msg.member.user.id);
+  const roles = guild.roles;
+
+  // Roles
+  const roleArtist = roles.find((role) => role.name === "artist");
+
+  if (channelId === channelIds.pickYourRole) {
+    if (msg.content === "😄") {
+      member.addRole(roleArtist);
+    }
+  }
+
   // if (msg.content === "ping") {
   //   msg.reply("pong");
   //   msg.channel.send("pong");
-  // } else if (msg.content.startsWith("!kick")) {
-  //   if (msg.mentions.users.size) {
-  //     const taggedUser = msg.mentions.users.first();
-  //     msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-  //   } else {
-  //     msg.reply("Please tag a valid user!");
-  //   }
   // }
 });
 
-bot.login(process.env.TOKEN);
+// You can also try to upgrade partials to full instances:
+client.on("messageReactionAdd", async (reaction, user) => {
+  console.log("reaction", reaction);
+  if (reaction.message.partial) await reaction.message.fetch();
+  if (reaction.partial) await reaction.fetch();
+  console.log(`${reaction.emoji.name}`);
 
-//
-// const Discord = require("discord.js");
-// const bot = new Discord.Client();
-// const TOKEN = process.env.TOKEN;
-//
-// bot.login(TOKEN);
-//
-// bot.on("ready", () => {
-//   console.info(`Logged in as ${bot.user.tag}!`);
-// });
-//
-// bot.on("message", (msg) => {
-//   if (msg.content === "ping") {
-//     msg.reply("pong");
-//     msg.channel.send("pong");
-//   } else if (msg.content.startsWith("!kick")) {
-//     if (msg.mentions.users.size) {
-//       const taggedUser = msg.mentions.users.first();
-//       msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-//     } else {
-//       msg.reply("Please tag a valid user!");
-//     }
-//   }
-// });
+  await reaction.remove(emojis.heart.id);
+
+  // switch (reaction.emoji.name) {
+  //   case expression:
+  //     break;
+  //   default:
+  // }
+});
+
+client.login(process.env.TOKEN);
