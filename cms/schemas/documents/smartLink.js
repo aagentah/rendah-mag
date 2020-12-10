1fae52c2231b112ace18fa1af503b0496fd7b543
danjonesdev <dannyjones360@gmail.com>
2020-12-10 17:35:32 +0000

    Add smart link and link in bio to CMS and website

---
+++ cms/schemas/documents/smartLink.js
@@ -0,0 +1,51 @@
+export default {
+  name: "smartLink",
+  title: "Smart Link",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (Rule) => Rule.required().max(60),
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "finalUrl",
+      title: "Final URL",
+      description: "Do not change value",
+      type: "slug",
+      options: {
+        source: "slug",
+        maxLength: 96,
+        slugify: (input) => `https://rendah.link/l/${input.current}`,
+      },
+    },
+    {
+      title: "Items",
+      name: "items",
+      type: "array",
+      of: [
+        {
+          title: "Item",
+          type: "smartLinkItem",
+        },
+      ],
+    },
+  ],
+  preview: {
+    select: {
+      title: "title",
+      media: "image",
+    },
+  },
+};
