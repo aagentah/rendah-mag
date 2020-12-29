19c0b39a9ba7fbdc1811b3e41553b57757968c22
danjonesdev <dannyjones360@gmail.com>
2020-12-29 13:11:38 +0000

    Ammends to dominion rss feed

--- website/lib/sanity/requests.js
+++ website/lib/sanity/requests.js
@@ -3,17 +3,6 @@ import sanityImage from '@sanity/image-url';
 import client, { previewClient } from './config';
 import dateTodayISO from '~/functions/dateTodayISO';
 
-const getUniquePosts = (posts) => {
-  const slugs = new Set();
-  return posts.filter((post) => {
-    if (slugs.has(post.slug)) {
-      return false;
-    }
-    slugs.add(post.slug);
-    return true;
-  });
-};
-
 const postFields = `
   name,
   title,
@@ -85,7 +74,7 @@ export async function getAllPosts(preview) {
     .fetch(`*[_type == "post"] | order(date desc, _updatedAt desc) [0..31] {
       ${postFields}
     }`);
-  return getUniquePosts(results);
+  return results;
 }
 
 export async function getAllPostsTotal(preview) {
@@ -93,7 +82,7 @@ export async function getAllPostsTotal(preview) {
     .fetch(`*[_type == "post"] | order(date desc, _updatedAt desc) {
       ${postFields}
     }`);
-  return getUniquePosts(results);
+  return results;
 }
 
 export async function getCategory(category, range) {
@@ -131,7 +120,7 @@ export async function getCurrentAndPreviousCyphers(preview) {
     ),
   ]);
 
-  return { current: current || null, previous: getUniquePosts(previous) };
+  return { current: current || null, previous: previous };
 }
 
 export async function getLatestAnouncedCypher(preview) {
@@ -157,7 +146,7 @@ export async function getTeamMembers(preview) {
     .fetch(`*[_type == "author" && active] | order(order asc){
       ${teamFields}
     }`);
-  return getUniquePosts(results);
+  return results;
 }
 
 export async function getTeamMemberAndPosts(slug, preview) {
@@ -178,7 +167,7 @@ export async function getAllProducts(preview) {
     .fetch(`*[_type == "storeItem"] | order(date desc, _updatedAt desc) {
       ${productFields}
     }`);
-  return getUniquePosts(results);
+  return results;
 }
 
 export async function getAllProductsTotal(preview) {
@@ -186,7 +175,7 @@ export async function getAllProductsTotal(preview) {
     .fetch(`*[_type == "storeItem"] | order(date desc, _updatedAt desc) {
       ${productFields}
     }`);
-  return getUniquePosts(results);
+  return results;
 }
 
 export async function getProduct(slug, preview) {
@@ -219,7 +208,7 @@ export async function getPostAndMore(slug, preview) {
       { slug }
     ),
   ]);
-  return { post, morePosts: getUniquePosts(morePosts) };
+  return { post, morePosts: morePosts };
 }
 
 export async function getPreviewProductBySlug(slug) {
@@ -254,7 +243,7 @@ export async function getProductAndMore(slug, preview) {
     ),
   ]);
 
-  return { product, moreProducts: getUniquePosts(moreProducts) };
+  return { product, moreProducts: moreProducts };
 }
 
 export async function getLatestMix(preview) {
@@ -270,24 +259,39 @@ export async function getMixes(preview) {
     .fetch(`*[_type == "mix"] | order(date desc, _updatedAt desc) [0..15] {
       ...,
     }`);
-  return getUniquePosts(results);
+  return results;
 }
 
-export async function getLatestDominionItem(preview) {
+export async function getLatestDominionItems(preview) {
   const results = await getClient(preview)
-    .fetch(`*[_type == "dominionItem"] | order(activeFrom desc) [0] {
+    .fetch(`*[_type == "dominionItem"] | order(activeFrom desc) [0..4] {
       ...,
     }`);
   return results;
 }
 
-export async function getDominionItemsSinceDate(sinceStartOfMonth) {
-  const results = await getClient().fetch(
-    `*[_type == "dominionItem" && activeFrom >= $sinceStartOfMonth] | order(activeFrom desc) {
-      ...,
-    }`,
-    { sinceStartOfMonth }
-  );
+export async function getDominionItemsSinceDate(sinceStartOfMonth, preview) {
+  const curClient = getClient(preview);
+
+  const [results, welcome] = await Promise.all([
+    curClient
+      .fetch(
+        `*[_type == "dominionItem" && slug.current != "welcome-to-the-dominion" && activeFrom >= $sinceStartOfMonth] | order(activeFrom desc) {
+        ...,
+        "slug": slug.current,
+      }`,
+        { sinceStartOfMonth }
+      )
+      .then((res) => res),
+    curClient.fetch(
+      `*[_type == "dominionItem" && slug.current == "welcome-to-the-dominion"] [0] {
+        ...,
+        "slug": slug.current,
+      }`
+    ),
+  ]);
+
+  results.push(welcome);
   return results;
 }
 
