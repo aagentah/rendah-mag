54c27e686667cd2abbb06746e83908a52ad3c59c
danjonesdev <dannyjones360@gmail.com>
2021-01-08 14:11:38 +0000

    Minors

--- website/constants/index.js
+++ website/constants/index.js
@@ -1,6 +1,2 @@
 // Snipcart
 export const IS_ECCOMERCE = true;
-
-export const SNIPCART_SECRET_KEY = process.env.SNIPCART_SECRET_KEY;
-
-export const SNIPCART_API_KEY = process.env.SNIPCART_API_KEY;
