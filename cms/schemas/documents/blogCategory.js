62d002b52ef3323731f76e74bea553dac45076a3
danjonesdev <dannyjones360@gmail.com>
2021-02-13 13:05:48 +0000

    Various site-wide amends

--- cms/schemas/documents/blogCategory.js
+++ cms/schemas/documents/blogCategory.js
@@ -1,29 +1,28 @@
 export default {
-  name: 'category',
-  title: 'Category',
-  type: 'document',
+  name: "category",
+  title: "Category",
+  type: "document",
   fields: [
     {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-      validation: Rule => Rule.required().max(60)
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (Rule) => Rule.required().max(60),
     },
     {
-      name: 'slug',
-      title: 'URL',
-      type: 'slug',
+      name: "slug",
+      title: "URL",
+      type: "slug",
       options: {
-        source: 'title',
-        maxLength: 96
+        source: "title",
+        maxLength: 96,
       },
-      validation: Rule => Rule.required()
+      validation: (Rule) => Rule.required(),
     },
     {
-      name: 'description',
-      title: 'Description',
-      type: 'text',
-      validation: Rule => Rule.required().min(50).max(300)
-    }
-  ]
-}
+      name: "description",
+      title: "Description",
+      type: "text",
+    },
+  ],
+};
