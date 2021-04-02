f571f8e5e4cd1604fc9e5a1cc2a5882fd8a35dd1
danjonesdev <dannyjones360@gmail.com>
2021-04-02 12:36:37 +0100

    Redesign quote module

--- website/components/article/body-sections/quote/index.js
+++ website/components/article/body-sections/quote/index.js
@@ -1,11 +1,20 @@
 export default function Quote({ quote, source }) {
   return (
     <>
-      <p className="bw2  bl  bc-light-grey  pa3">
-        <q className="t-body  lh-copy  f5  dark-grey  di  taj">{quote}</q>
-        <span className="t-body  lh-copy  f6  dark-grey  fs-normal  di  tac  pt2  pl2">
-          - {source}
-        </span>
+      <p className="flex  flex-wrap  justify-center  pt4">
+        <div className="col-24">
+          <q className="t-body  lh-copy  f5  dark-grey  tac  fs-italic  db">
+            {quote}
+          </q>
+        </div>
+        <div className="col-24">
+          <span className="t-body  lh-copy  f5  grey  tac  db">~~~</span>
+        </div>
+        <div className="col-24">
+          <span className="t-body  lh-copy  f5  dark-grey  fw7  tac  db">
+            {source}
+          </span>
+        </div>
       </p>
     </>
   );
