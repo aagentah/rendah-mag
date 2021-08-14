2b3870ab1efca8fce0569c719884b93895803644
danjonesdev <dannyjones360@gmail.com>
2021-08-14 16:11:15 +0100

    Minors

--- website/lib/sanity/requests.js
+++ website/lib/sanity/requests.js
@@ -363,7 +363,7 @@ export async function getSmartLinksTotal(preview) {
 export async function getLinkInBio(preview) {
   const results = await getClient(preview).fetch(
     `*[_type == "linkInBio"] [0] {
-      'items': items[] {
+      'items': items[] [0..11] {
         'field': field {
           'documentInternal': documentInternal {
             'document': *[_id == ^._ref] [0] {
