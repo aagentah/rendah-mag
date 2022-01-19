3fa9f9184c516896b2a999bc9ea05e346e7a7c78
danjonesdev <dannyjones360@gmail.com>
2022-01-19 09:54:51 +0000

    Switch GA for Plausible

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -3,9 +3,9 @@ import Router from 'next/router';
 import { PageTransition } from 'next-page-transitions';
 import NProgress from 'nprogress';
 import { ParallaxProvider } from 'react-scroll-parallax';
+import PlausibleProvider from 'next-plausible';
 
 import { AppProvider } from '~/context-provider/app';
-import * as gtag from '~/lib/gtag';
 
 import '~/styles/index.scss';
 import 'keen-slider/keen-slider.min.css';
@@ -14,9 +14,6 @@ import 'react-toastify/dist/ReactToastify.css';
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
-
-// Track pageview when route is changed
-Router.events.on('routeChangeComplete', (url) => gtag.pageview(url));
 
 // Performance metrics
 // export function reportWebVitals(metric) {
@@ -28,23 +25,25 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <AppProvider>
-        <ParallaxProvider>
-          <PageTransition
-            timeout={transitionTimeout}
-            classNames="page-transition"
-            loadingComponent={null}
-            loadingDelay={transitionTimeout}
-            loadingTimeout={{
-              enter: transitionTimeout,
-              exit: transitionTimeout,
-            }}
-            loadingClassNames="loading-indicator"
-          >
-            <Component {...pageProps} />
-          </PageTransition>
-        </ParallaxProvider>
-      </AppProvider>
+      <PlausibleProvider domain="rendahmag.com">
+        <AppProvider>
+          <ParallaxProvider>
+            <PageTransition
+              timeout={transitionTimeout}
+              classNames="page-transition"
+              loadingComponent={null}
+              loadingDelay={transitionTimeout}
+              loadingTimeout={{
+                enter: transitionTimeout,
+                exit: transitionTimeout,
+              }}
+              loadingClassNames="loading-indicator"
+            >
+              <Component {...pageProps} />
+            </PageTransition>
+          </ParallaxProvider>
+        </AppProvider>
+      </PlausibleProvider>
     </>
   );
 }
--- website/pages/_document.js
+++ website/pages/_document.js
@@ -1,7 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 import { IS_ECCOMERCE } from '~/constants';
-import { GA_TRACKING_ID } from '~/lib/gtag';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -53,27 +52,6 @@ export default class MyDocument extends Document {
           <meta name="theme-color" content="#000" />
 
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-
-          <>
-            {/* Global Site Tag (gtag.js) - Google Analytics */}
-            <script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-            />
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-                      window.dataLayer = window.dataLayer || [];
-                      function gtag(){dataLayer.push(arguments);}
-                      gtag('js', new Date());
-
-                      gtag('config', '${GA_TRACKING_ID}', {
-                        page_path: window.location.pathname,
-                      });
-                    `,
-              }}
-            />
-          </>
         </Head>
 
         <div
