fc5ac75d6d032e945924345510b1528d56bbed6e
danjonesdev <dannyjones360@gmail.com>
2022-02-19 13:27:03 +0000

    Add guest mixes to home page

--- website/pages/index.js
+++ website/pages/index.js
@@ -20,10 +20,13 @@ export default function Home({ siteConfig }) {
   const [news, setNews] = useState(null);
   const [insights, setInsights] = useState(null);
   const [premieres, setPremieres] = useState(null);
+  const [guestMixes, setGuestMixes] = useState(null);
+
   const [interviewsLength, setInterviewsLength] = useState(4);
   const [newsLength, setNewsLength] = useState(6);
   const [insightsLength, setInsightsLength] = useState(6);
   const [premieresLength, setPremieresLength] = useState(4);
+  const [guestMixesLength, setGuestMixesLength] = useState(4);
 
   useEffect(() => {
     const action = async () => {
@@ -32,12 +35,14 @@ export default function Home({ siteConfig }) {
       const newsRes = await getCategory('news', [1, 6]);
       const insightsRes = await getCategory('insights', [1, 6]);
       const premieresRes = await getCategory('premieres', [1, 4]);
+      const guestMixesRes = await getCategory('guest-mix', [1, 4]);
 
       setFeaturedPost(featuredPostRes);
       setInterviews(interviewsRes);
       setNews(newsRes);
       setInsights(insightsRes);
       setPremieres(premieresRes);
+      setGuestMixes(guestMixesRes);
     };
 
     action();
@@ -204,7 +209,7 @@ export default function Home({ siteConfig }) {
           <Container>
             <div className="flex  flex-wrap">
               <div className="col-24  col-12-md  pr0  pr3-md">
-                <section className="pb5  pb6-md">
+                <section className="pb5">
                   <div className="pb4  ph3">
                     <div className="bg-black  pa2  dib">
                       <Heading
@@ -266,7 +271,7 @@ export default function Home({ siteConfig }) {
               </div>
 
               <div className="col-24  col-12-md  pl0  pl3-md">
-                <section className="pb5  pb6-md">
+                <section className="pb5">
                   <div className="pb4  ph3">
                     <div className="bg-black  pa2  dib">
                       <Heading
@@ -319,6 +324,74 @@ export default function Home({ siteConfig }) {
                         routerLink: Link,
                         routerLinkProps: {
                           as: `/category/insights`,
+                          scroll: false,
+                        },
+                      }}
+                    />
+                  </div>
+                </section>
+              </div>
+            </div>
+          </Container>
+
+          <Container>
+            <div className="flex  flex-wrap">
+              <div className="col-24">
+                <section className="pb5  pb6-md">
+                  <div className="pb4  ph3">
+                    <div className="bg-black  pa2  dib">
+                      <Heading
+                        /* Options */
+                        htmlEntity="h2"
+                        text="Guest Mixes"
+                        color="white"
+                        size="small"
+                        truncate={null}
+                        /* Children */
+                        withLinkProps={null}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flex  flex-wrap">
+                    {[...Array(premieresLength)].map((iteration, i) => (
+                      <div key={iteration} className="col-24  col-6-md">
+                        <div className="ph3  pv2">
+                          <CardBlog
+                            i={i}
+                            post={
+                              guestMixes?.articles && guestMixes?.articles[i]
+                            }
+                            columnCount={4}
+                          />
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+
+                  <div className="flex  justify-end  pr2">
+                    <Button
+                      /* Options */
+                      type="secondary"
+                      size="medium"
+                      text="All Guest Mixes"
+                      color="black"
+                      fluid={false}
+                      icon={buttonIcon}
+                      iconFloat={null}
+                      inverted={false}
+                      loading={false}
+                      disabled={false}
+                      skeleton={false}
+                      onClick={null}
+                      /* Children */
+                      withLinkProps={{
+                        type: 'next',
+                        href: '/category/[slug]',
+                        target: null,
+                        routerLink: Link,
+                        routerLinkProps: {
+                          as: `/category/guest-mix`,
                           scroll: false,
                         },
                       }}
