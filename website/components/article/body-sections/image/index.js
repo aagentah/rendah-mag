dd85e5c8ec50473b8b18b1b0703c60c0855a24c4
Dan Jones <danjones@CTI894s-MacBook-Pro.local>
2022-04-08 19:14:17 +0100

    Minors

--- website/components/article/body-sections/image/index.js
+++ website/components/article/body-sections/image/index.js
@@ -2,6 +2,7 @@ import { Image } from 'next-pattern-library';
 import LazyLoad from 'react-lazyload';
 import BlockContent from '@sanity/block-content-to-react';
 import isArray from 'lodash/isArray';
+import includes from 'lodash/includes';
 
 import { imageBuilder } from '~/lib/sanity/requests';
 import { SANITY_BLOCK_SERIALIZERS } from '~/constants';
@@ -41,6 +42,17 @@ export default function ImageSection({ section }) {
     }
   };
 
+  const placeholder = includes(section.asset._ref, '-gif')
+    ? null
+    : imageBuilder
+        .image(section.asset)
+        .height(25)
+        .width(25)
+        .auto('format')
+        .fit('clip')
+        .blur('20')
+        .url();
+
   return (
     <LazyLoad once offset={100} height={360}>
       <figure>
@@ -51,14 +63,7 @@ export default function ImageSection({ section }) {
             .auto('format')
             .fit('clip')
             .url()}
-          placeholder={imageBuilder
-            .image(section.asset)
-            .height(25)
-            .width(25)
-            .auto('format')
-            .fit('clip')
-            .blur('20')
-            .url()}
+          placeholder={placeholder}
           alt="This is the alt text."
           figcaption={null}
           height={null}
