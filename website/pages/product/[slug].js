a40c0340d2bad556f5554fc6a402b70b1b3bd023
Dan <djmcr@protonmail.com>
2023-01-07 17:53:20 +0000

    Minors

--- website/pages/product/[slug].js
+++ website/pages/product/[slug].js
@@ -393,7 +393,7 @@ export default function Product({ siteConfig, product }) {
                   />
                 </div>
                 <div className="col-24  col-12-md  pl4-md">
-                  <div className="db  pb3  ph2">
+                  <div className="dib  pb3  pl2  pr3">
                     <Heading
                       /* Options */
                       htmlEntity="h1"
@@ -405,35 +405,37 @@ export default function Product({ siteConfig, product }) {
                       withLinkProps={null}
                     />
                   </div>
-                  <div className="flex  flex-wrap  ph2  pb3">
+
+                  {product?.tag && product?.tag !== 'None' && (
                     <div className="dib  pr2">
                       <Label
                         /* Options */
-                        customClass="bold  ba  bc-rendah-red"
-                        text={`£${product?.price}`}
-                        color="rendah-red"
-                        backgroundColor={null}
-                        skeleton={!product}
+                        customClass="bold  ba  bc-black"
+                        text={product?.tag}
+                        color="black"
+                        backgroundColor=""
                         onClick={null}
                         /* Children */
                         withLinkProps={null}
                       />
                     </div>
+                  )}
+                  <div className="flex  flex-wrap  ph2  pb3">
+                    <div className="dib  pr2">
+                      <p className="dib  black  f5  lh-copy  fw7">
+                        £{product?.price}
+                      </p>
 
-                    {product?.tag && product?.tag !== 'None' && (
-                      <div className="dib  pr2">
-                        <Label
-                          /* Options */
-                          customClass="ph2"
-                          text={product?.tag}
-                          color="black"
-                          backgroundColor="white"
-                          onClick={null}
-                          /* Children */
-                          withLinkProps={null}
-                        />
-                      </div>
-                    )}
+                      <p className="dib  rendah-red  f6  lh-copy  pl2">
+                        (or £9 + free shipping on the{' '}
+                        <Link href="/dominion" legacyBehavior>
+                          <span className="cp  rendah-red  underline">
+                            Dominion Subscription
+                          </span>
+                        </Link>
+                        )
+                      </p>
+                    </div>
                   </div>
 
                   <div className="product__tabs  bb  bc-black  mb3">
