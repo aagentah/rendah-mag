2251c9d13a28f1d20059dcec7d9f81e813c58fb2
Dan <djmcr@protonmail.com>
2023-08-01 17:29:33 +0100

    Minors

--- website/pages/rss/html/blog-latest.xml.js
+++ website/pages/rss/html/blog-latest.xml.js
@@ -5,19 +5,27 @@ import { getAllPosts, imageBuilder } from '~/lib/sanity/requests';
 import escapeXml from '~/functions/escapeXml';
 import encodeSpecialChar from '~/functions/encodeSpecialChar';
 
+const isEven = (number) => {
+  return number % 2 === 0;
+};
+
 const sitemapXml = (posts) => {
   if (posts.length) {
     let postsXML = '';
 
-    posts.map((post) => {
+    posts.map((post, i) => {
       const title = post?.title;
 
       const titleBlock = post?.title
-        ? `<h2 style="font-weight: bold; text-align: left;">${title}</h2>`
+        ? `<h2 style="font-weight: bold; text-align: left;  ${
+            isEven(i) ? 'color: #d9d9d9;' : ''
+          }">${title}</h2>`
         : '';
 
       const tagLineBlock = post?.socialTagline
-        ? `<p style="color: #000000; text-align: left;">${post.socialTagline}</p>`
+        ? `<p style="color: #000000; text-align: left;  ${
+            isEven(i) ? 'color: #d9d9d9;' : ''
+          }">${post.socialTagline}</p>`
         : '';
 
       const url = post?.slug
@@ -25,15 +33,28 @@ const sitemapXml = (posts) => {
         : process.env.SITE_URL;
 
       const date = new Date(post?.publishedAt).toUTCString();
-
-      const readMoreLink = `<p><a style="color: #000000; text-decoration: underline; text-align: left;" href="${url}" target="_blank">Read full article</a></p>`;
+      const readMoreLink = `<p><a style="color: #000000; text-decoration: underline; text-align: left;  ${
+        isEven(i) ? 'color: #d9d9d9;' : ''
+      }" href="${url}" target="_blank">Read full article</a></p>`;
 
       const html = `
-      <table cellspacing="0" cellpadding="0" border="0" width="100%">
+      <table cellspacing="0" cellpadding="0" border="0" width="100%" ${
+        isEven(i) ? `style="background: #1c1c1c; color: #d9d9d9;` : ''
+      }>
+        ${
+          isEven(i)
+            ? `
+            <tr>
+              <td><br /></td>
+            </tr>
+              `
+            : `
+              `
+        }
         <tr>
-          <td width="300" valign="top">
-            <img width="300" style="width: 300px;" src="${imageBuilder
-              .image(post.image)
+          <td width="300" valign="top" style="border-radius: 1rem;">
+            <img width="300" style="width: 300px; border-radius: 1rem;" src="${imageBuilder
+              .image(post.coverImage)
               .height(300)
               .width(300)
               .auto('format')
@@ -50,6 +71,16 @@ const sitemapXml = (posts) => {
             ${readMoreLink}
           </td>
         </tr>
+        ${
+          isEven(i)
+            ? `
+            <tr>
+              <td><br /></td>
+            </tr>
+              `
+            : `
+              `
+        }
         <tr>
           <td><br /></td>
         </tr>
