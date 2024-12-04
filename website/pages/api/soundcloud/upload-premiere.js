42e44bb26311447a2e596db539d756b8d473c15b
Dan <djmcr@protonmail.com>
2024-12-04 23:25:17 +0000

    Update log

--- website/pages/api/soundcloud/upload-premiere.js
+++ website/pages/api/soundcloud/upload-premiere.js
@@ -66,6 +66,15 @@ const getAccessToken = async () => {
   }
 };
 
+const streamToBuffer = async (stream) => {
+  const chunks = [];
+  return new Promise((resolve, reject) => {
+    stream.on('data', (chunk) => chunks.push(chunk));
+    stream.on('error', reject);
+    stream.on('end', () => resolve(Buffer.concat(chunks)));
+  });
+};
+
 const handler = async (req, res) => {
   await cors(req, res);
 
