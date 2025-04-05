3f9ca1117fa42ad88c287e793ebf22ebfb8d8a5e
Dan <djmcr@protonmail.com>
2025-04-05 22:44:32 +0100

    Minors

--- website/pages/profile/index.js
+++ website/pages/profile/index.js
@@ -263,7 +263,7 @@ export default function Profile({ siteConfig }) {
                           : 'text-neutral-300'
                       }`}
                     >
-                      <span className="opacity-30">Assembly [WIP]</span>
+                      <span className="opacity-30">Coming Soon</span>
                     </button>
                     {/* <button
                           disabled
