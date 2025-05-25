ac0b66c744f8baf3c9aa091f15ce1ea3faed8d0d
Dan <djmcr@protonmail.com>
2025-05-25 20:57:24 +0100

    Pixel

--- website/pages/_document.js
+++ website/pages/_document.js
@@ -87,9 +87,44 @@ export default class MyDocument extends Document {
               }}
             />
           )}
+
+          {/* Meta Pixel (only in Production) */}
+          {isProduction && (
+            <>
+              {/* Meta Pixel Script: Facebook/Meta tracking, loaded after page interactive for analytics */}
+              <Script
+                id="meta-pixel"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                  __html: `!function(f,b,e,v,n,t,s)
+                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                  n.queue=[];t=b.createElement(e);t.async=!0;
+                  t.src=v;s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)}(window, document,'script',
+                  'https://connect.facebook.net/en_US/fbevents.js');
+                  fbq('init', '2984009378374748');
+                  fbq('track', 'PageView');`,
+                }}
+              />
+            </>
+          )}
         </Head>
 
         <body>
+          {/* Meta Pixel noscript fallback: ensures tracking for users with JS disabled */}
+          {isProduction && (
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                style={{ display: 'none' }}
+                src="https://www.facebook.com/tr?id=2984009378374748&ev=PageView&noscript=1"
+                alt="Meta Pixel"
+              />
+            </noscript>
+          )}
           <Main />
           <NextScript />
         </body>
